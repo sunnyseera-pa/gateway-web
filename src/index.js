5d3c5dad9b46eefa24f47436fdb0959f7c0be46f
Paul McCafferty <Paul.McCafferty@PACONSULTING.COM>
2020-02-12 16:39:21 +0000

    RDT3-97 - Checking in Person,Project and Tool pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HDRRouter from './HDRRouter';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './css/custom-css-bootstrap-magic-2020-02-10.css';
+import './css/hdr.css';
+
+ReactDOM.render(<HDRRouter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
