6fa6c988e7ff3004df8dcf12d8a85afbfd1a50c4
Ciara <ciara.ward@paconsulting.com>
2020-02-13 09:15:40 +0000

    RDT3-67 landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 import Row  from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card, { CardBody } from 'react-bootstrap/Card';
-import '../css/landingPage.css';
+import Card from 'react-bootstrap/Card';
+import '../css/hdruk.css';
 import SVGIcon from '../images/SVGIcon';
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
@@ -12,80 +12,31 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 class LandingPage extends React.Component{
     render(){
         return(
-            // <div>
-            //     <style>{'body {background-color: #eeeeee;}'}</style>
-
-            // </div>
-
-            // <div>
-            // <Container>
-
-            // </Container>
-
-            // <Container>
-
-            // </Container>
-            // </div>
-
-            // <Container>
-            //     <Row style={{minHeight: 512, width: 1440}}>
-            //         <style>{'body {background-color: #eeeeee;}'}</style>
-            //     </Row>
-            //     <Row style={{minHeight: 512, width: 1440}}>
-            //      <style>{'body {background-color: #2c8267;}'}</style>
-            //     </Row>
-            // </Container>
-
-        // <Container id="mainLandingPage">
-        //     <style>{'body {background-color: #fffff;}'}</style>
-        //     <Row id="landingPageTop">
-        //         <style>{'body {background-color: #475da7;}'}</style>
-        //     </Row>
-        //     <Row id="landingPageBottom">
-        //      <style>{'body {background-color: #2c8267;}'}</style>
-        //     </Row>
-        // </Container>
-
-        // <Container >
-        // <style>{'body {background-color: #fffff;}'}</style>
-        // <Row style={{height: '20%'}}>
-        //     <style>{'body {background-color: #475da7;}'}</style>
-        // </Row>
-        // <Row style={{height: '80%'}}>
-        // <style>{'body {background-color: #2c8267;}'}</style>
-        // </Row>
-        // </Container>
-
-        <Container >
+        <Container className="LandingBackground">
+        <Row id="landingPageEmptyRow"></Row>
         <Row>
-            <Col sm={12}>
-            <ButtonToolbar>
-                <Button variant="primary">Primary</Button>
-                <Button variant="secondary">Secondary</Button>
-                <Button variant="success">Success</Button>
-                <Button variant="warning">Warning</Button>
-                <Button variant="danger">Danger</Button>
-                <Button variant="info">Info</Button>
-                <Button variant="light">Light</Button>
-                <Button variant="dark">Dark</Button>
-            </ButtonToolbar>
+            <Col sm={2}></Col>
+            <Col sm={8}>
+            <Card id="landingPageCard" className="bg-transparent mb-2" border="0" >
+                <Card.Text>
+                Explore tools, resources and code used in health
+                 research across the UK
+                </Card.Text>
+            </Card>
             </Col>
+            <Col sm={2}></Col>
         </Row>
         <Row>
         <Col sm={12}>
-                <nav class="navbar navbar-expand-md bg-white justify-content-center">
                     <div>
-                    {/* <SearchIcon type="submit"> Submit </SearchIcon> */}
-                    <form class="form-inline" action="">
-                        <div id="searchInputHolder" class="form-control mr-md-2"> 
+                    <form className="form-inline" action="">
+                        <div id="searchInputHolder" className="form-control mr-md-2"> 
                         <SVGIcon name="searchicon" width={17} height={17} fill={'#2c8267'} stroke='none' type="submit"/>
-                         {/* <SearchIcon id="searchIcon" type="submit"> Submit </SearchIcon> */}
                         <input id="searchInput" type="text" placeholder=" Search" style={{border: 0, flex: 0.97}}> 
                         </ input>
                         </div>
                     </form>
                     </div>
-                </nav>
                 </Col>
         </Row>
         <Row>
@@ -100,4 +51,3 @@ class LandingPage extends React.Component{
 
 export default LandingPage;
 
-// #475da7
--- src/pages/components/searchBar.js
+++ src/pages/components/searchBar.js
@@ -8,54 +8,18 @@ import Col from 'react-bootstrap/Col';
 // import {ReactComponent as SearchIcon} from '../images/search.svg';
 import FormGroup from 'react-bootstrap/FormGroup';
 import Container from 'react-bootstrap/Container';
-import '../../css/searchBar.css';
+import '../../css/hdruk.css';
 import SVGIcon from "../../images/SVGIcon"
 
 class SearchBar extends React.Component{
     render(){
         return(
-                //     <div style={{ padding: '10px' }}>
-                //         <input
-                //     type="text"
-                //     onChange={(e) => this.setState({ search: e.target.value })}
-                //     placeholder="search"
-                //     style={{ width: '200px' }}
-                //   />
-                //   <br />
-                //   <button onClick={() => this.getDataSearchFromDb(this.state.search)}>
-                //     SEARCH
-                //   </button>
-                //     </div>
-
-                // <Navbar className="bg-white">
-                // <Form inline>
-                //     {/* <Form.Row> */}
-                //     {/* <InputGroup.SearchIcon></InputGroup.SearchIcon> */}
-                //     {/* <FormGroup> */}
-                //     <InputGroup>
-                //         {/* <InputGroup.Prepend>
-                //             <SearchIcon width="40" height="40" type="submit"> Submit </SearchIcon>
-                //         </InputGroup.Prepend> */}
-                //         {/* <img src= './images/search.svg' /> */}
-                //         <SearchIcon type="submit"> Submit </SearchIcon>
-                //         <FormControl type="text" placeholder="Search" className="bg-light mr-sm-2">
-                //             {/* <SearchIcon type="submit"> Submit </SearchIcon> */}
-                //         </FormControl>
-                //         {/* <FormControl type="text" placeholder="Search" className="bg-light mr-sm-2"> */}
-                //             {/* <option> <SearchIcon type="submit"> Submit </SearchIcon> </option>  */}
-                //         {/* </FormControl> */}
-                //     </InputGroup>
-                // </Form>
-                // </Navbar>
-
                 <Col sm={12}>
                 <nav class="navbar navbar-expand-md bg-white justify-content-center">
                     <div>
-                    {/* <SearchIcon type="submit"> Submit </SearchIcon> */}
                     <form class="form-inline" action="">
                         <div id="searchInputHolder" class="form-control mr-md-2"> 
                         <SVGIcon name="searchicon" width={17} height={17} fill={'#2c8267'} stroke='none' type="submit"/>
-                         {/* <SearchIcon id="searchIcon" type="submit"> Submit </SearchIcon> */}
                         <input id="searchInput" type="text" placeholder=" Search" style={{border: 0, flex: 0.97}}> 
                         </ input>
                         </div>
