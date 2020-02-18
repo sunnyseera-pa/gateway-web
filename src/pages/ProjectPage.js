bd9850e76b8ed2b4730c4309126b406e84babab4
Paul McCafferty <Paul.McCafferty@PACONSULTING.COM>
2020-02-18 17:16:48 +0000

    RDT3-68 - Updates to the redirect code

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -18,7 +18,7 @@ import Col from 'react-bootstrap/Col';
 var baseURL = window.location.href;
 
 if (!baseURL.includes('localhost')) {
-    var rx = /^([http|https]+\:\/\/[a-z]+)(.*)/;
+    var rx = /^([http|https]+\:\/\/[a-z]+)([^/]*)/;
     var arr = rx.exec(baseURL);
     if (arr.length > 0) {
         //add -api to the sub domain for API requests
