a92217239972012a167dc0add9398ea1ab92018e
Tony Espley <tony.espley@paconsulting.com>
2020-03-02 08:06:34 +0000

    RDT3 - Bugfix attempt for -api multiple times

--- src/BaseURL.js
+++ src/BaseURL.js
@@ -1,11 +1,11 @@
 
-var baseURL = window.location.href;
+var thisBaseURL = window.location.href;
 
 module.exports = {
   getURL: function() {
-    if (!baseURL.includes('localhost')) {
+    if (!thisBaseURL.includes('localhost')) {
       var rx = /^([http|https]+\:\/\/[a-z]+)([^/]*)/;
-      var arr = rx.exec(baseURL);
+      var arr = rx.exec(thisBaseURL);
       if (arr.length > 0) {
           //add -api to the sub domain for API requests
           return arr[1]+'-api'+arr[2]
