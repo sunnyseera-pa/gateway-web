0f81b90d4da43805e90310a2a3c3d44061be3e61
Diego <diego.frison@paconsulting.com>
2020-05-15 15:29:59 +0100

    Merge pull request #1 from HDRUK/RDT3-249
    RDT3-249 Discourse API integration

---
+++ src/pages/commonComponents/DiscourseAddPost.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+
+var baseURL = require('./BaseURL').getURL();
+
+class DiscourseAddPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state.topicLink = props.topicLink;
+    this.state.userState = props.userState;
+    this.state.toolId = props.toolId;
+  }
+
+  // initialize our state
+  state = {
+    userState: [],
+    topicLink: null,
+    toolId: null
+  };
+  
+  createTopicAndRedirecToDiscourse = async (toolId) => {
+  
+    if (!toolId) {
+      return;
+    }
+    const res = await axios.put(baseURL + `/api/v1/discourse/topic/tool/${toolId}`);
+    this.setState({ topicLink: res.data.data.link });
+    window.open(res.data.data.link);
+  }
+
+  render() {
+    const { topicLink, toolId, userState } = this.state;
+    return (userState[0].loggedIn === true &&
+      <Button variant='light' id='discourse-add-comment' className='mb-1' onClick={() => {
+        topicLink ? window.open(topicLink) : this.createTopicAndRedirecToDiscourse(toolId)
+      }}>
+        + Add a comment
+      </Button>
+    );
+  }
+}
+
+
+export default DiscourseAddPost;
