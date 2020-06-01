ae861a6e94de3bb7735f4f426b8842f7d79891d6
“Richard <Richard.Hobbs@PACONSULTING.COM>
2020-06-01 16:29:33 +0100

    Adding notification dropdown

--- src/pages/commonComponents/SearchBar.js
+++ src/pages/commonComponents/SearchBar.js
@@ -1,15 +1,44 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Alert from 'react-bootstrap/Alert';
+import Dropdown from 'react-bootstrap/Dropdown';
+import NotificationBadge from 'react-notification-badge';
+import { Effect } from 'react-notification-badge';
 
 import SVGIcon from "../../images/SVGIcon";
 import { ReactComponent as ColourLogoSvg } from '../../images/colour.svg';
 import { ReactComponent as ClearButtonSvg } from '../../images/clear.svg';
+import { ReactComponent as NotificationsBellSvg } from '../../images/bell.svg';
 
+import Messages from '../dashboard/NotificationMessages';
 import UserMenu from './UserMenu';
+
+let toggleOn = false;
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a href="" ref={ref} onClick={e => { e.preventDefault(); onClick(e); }} style={{ float: "right" }}>
+        {children}
+
+    </a>
+));
+
+const CustomMenu = React.forwardRef(
+    ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+        const [value] = useState('');
+
+        return (
+            <div ref={ref} style={style} className={className} aria-labelledby={labeledBy} style={{ left: "200px", top: "86px", overflowY: 'scroll', overflowX: "hidden", maxHeight: "432px", maxWidth: "400px" }}>
+                <ul className="list-unstyled">
+                    {React.Children.toArray(children).filter(
+                        child =>
+                            !value || child.props.children.toLowerCase().startsWith(value),
+                    )}
+                </ul>
+            </div>
+        );
+    },
+);
 
 var baseURL = require('./BaseURL').getURL();
 var cmsURL = require('./BaseURL').getCMSURL();
@@ -23,17 +52,47 @@ class SearchBar extends React.Component {
             role: "Reader",
             id: null,
             name: null
-        }]
+        }],
+        dropdownOpen: false,
+        count: 3
     }
 
     constructor(props) {
         super(props);
         this.state.userState = props.userState;
+        this.toggle = this.toggle.bind(this);
     }
 
     changeText = (e) => {
         this.setState({ textValue: e.target.value });
         this.props.doUpdateSearchString(e.target.value);
+    }
+
+    toggle(e) {
+        this.setState(prevState => ({
+            dropdownOpen: !prevState.dropdownOpen
+        }));
+    }
+
+    componentWillMount() {
+        document.addEventListener('mousedown', this.handleClick, false);
+    }
+
+    componentWillUnmount() {
+        document.addEventListener('mousedown', this.handleClick, false);
+    }
+
+    handleClick = (e) => {
+        try {
+            if (this.node.contains(e.target)) {
+                return;
+            }
+            else {
+                this.setState({ dropdownOpen: false });
+            }
+        } catch (e) {
+            this.setState({ dropdownOpen: false });
+        }
     }
 
     render() {
@@ -58,9 +117,11 @@ class SearchBar extends React.Component {
                                                 <span className="SearchInputIconGrey">
                                                     <SVGIcon name="searchicon" width={20} height={20} fill={'#2c8267'} stroke='none' type="submit" />
                                                 </span>
-                                                <span>
+
+                                                <span className="SearchInputIconGrey">
                                                     <input type="text" placeholder="Search" id="SearchInputSpanGrey" onChange={this.changeText} onKeyDown={this.props.doSearchMethod} value={this.props.searchString} />
                                                 </span>
+
                                                 {(this.props.searchString != '' && this.props.searchString != undefined) ?
                                                     <span className="SearchInputClearGrey">
                                                         <a style={{ cursor: 'pointer' }} href={'/search?search='} >
@@ -68,6 +129,31 @@ class SearchBar extends React.Component {
                                                         </a>
                                                     </span> : null}
                                             </span>
+                                        </Col>
+
+
+
+                                        <Col >{
+                                            this.state.userState[0].loggedIn ?
+                                                <Dropdown ref={node => this.node = node} isOpen={this.state.dropdownOpen} onClick={this.toggle} style={{ paddingTop: "26px", left: "100px" }}>
+                                                    <Dropdown.Toggle as={CustomToggle} variant="Success" id="NotificationsBell" style={{ float: "right" }} >
+                                                        {/* <span className="landingPageAccountText">{userState[0].name}</span> */}
+
+                                                        <span className="accountDropDownGap"></span>
+                                                        < NotificationsBellSvg width={50} height={50} id="NotificationsBell" className={this.state.dropdownOpen ? "NotificationsBell" : null} />
+
+                                                        <div >
+                                                            <NotificationBadge count={this.state.count} effect={Effect.SCALE} style={{ backgroundColor: '#29235c', top: '-50px', left: '44px', bottom: '', right: '' }} />
+                                                        </div>
+                                                    </Dropdown.Toggle>
+
+                                                    <Dropdown.Menu as={CustomMenu} style={{ overflowY: 'scroll', overflowX: "hidden", maxHeight: "432px", maxWidth: "400px" }}>
+                                                        <Messages userState={userState} />
+
+
+                                                    </Dropdown.Menu>
+                                                </Dropdown> : null
+                                        }
                                         </Col>
                                     </Row>
                                 </Container>
