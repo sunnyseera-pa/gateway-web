f532bd9bb2d966169f33cbfe14a24682c08b63da
“Richard <Richard.Hobbs@PACONSULTING.COM>
2020-06-03 13:20:30 +0100

    Resolving conflicts

--- src/pages/commonComponents/SearchBar.js
+++ src/pages/commonComponents/SearchBar.js
@@ -13,6 +13,8 @@ import { ReactComponent as NotificationsBellSvg } from '../../images/bell.svg';
 
 import Messages from '../dashboard/NotificationMessages';
 import UserMenu from './UserMenu';
+import { cmsURL } from '../../configs/url.config';
+
 
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
     <a href="" ref={ref} onClick={e => { e.preventDefault(); onClick(e); console.log('toggle was clicked'); }} style={{ float: "right", right: "700px" }}>
@@ -39,7 +41,6 @@ const CustomMenu = React.forwardRef(
 );
 
 var baseURL = require('./BaseURL').getURL();
-var cmsURL = require('./BaseURL').getCMSURL();
 
 class SearchBar extends React.Component {
 
@@ -61,7 +62,7 @@ class SearchBar extends React.Component {
         this.toggle = this.toggle.bind(this);
     }
 
-    changeText = (e) => {
+    onSearch = (e) => {
         this.setState({ textValue: e.target.value });
         this.props.doUpdateSearchString(e.target.value);
     }
@@ -95,71 +96,69 @@ class SearchBar extends React.Component {
 
     render() {
         const { userState } = this.state;
+
         return (
-            <>
-                <div className="searchBarBackground">
-                    <Row className="WhiteBackground">
-                        <Col xs={{ span: 6, order: 1 }} lg={{ span: 2, order: 1 }}>
-                            <div>
-                                <a style={{ cursor: 'pointer' }} href={cmsURL} >
-                                    <ColourLogoSvg className="ml-4 mt-3" />
-                                </a>
-                            </div>
-                        </Col>
-                        <Col xs={{ span: 12, order: 3 }} lg={{ span: 8, order: 2 }}>
-                            <div>
-                                <Container>
-                                    <Row>
-                                        <Col>
-                                            <span className="SearchBarInputGrey">
-                                                <span className="SearchInputIconGrey">
-                                                    <SVGIcon name="searchicon" width={20} height={20} fill={'#2c8267'} stroke='none' type="submit" />
-                                                </span>
-
-                                                <span className="SearchInputIconGrey">
-                                                    <input type="text" placeholder="Search" id="SearchInputSpanGrey" onChange={this.changeText} onKeyDown={this.props.doSearchMethod} value={this.props.searchString} />
-                                                </span>
-
-                                                {(this.props.searchString != '' && this.props.searchString != undefined) ?
-                                                    <span className="SearchInputClearGrey">
-                                                        <a style={{ cursor: 'pointer' }} href={'/search?search='} >
-                                                            <ClearButtonSvg />
-                                                        </a>
-                                                    </span> : null}
+            <div className="searchBarBackground">
+                <Row className="WhiteBackground">
+                    <Col xs={{ span: 6, order: 1 }} lg={{ span: 2, order: 1 }}>
+                        <div>
+                            <a style={{ cursor: 'pointer' }} href={cmsURL} >
+                                <ColourLogoSvg className="ml-4 mt-3" />
+                            </a>
+                        </div>
+                    </Col>
+                    <Col xs={{ span: 12, order: 3 }} lg={{ span: 8, order: 2 }}>
+                        <div>
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <span className="searchBarInputGrey">
+                                            <span className="searchInputIconGrey">
+                                                <SVGIcon name="searchicon" width={20} height={20} fill={'#2c8267'} stroke='none' type="submit" />
                                             </span>
-                                        </Col>
-                                        <Col >{
-                                            this.state.userState[0].loggedIn ?
-                                                <Dropdown ref={node => this.node = node} isOpen={this.state.dropdownOpen} onClick={this.toggle} style={{ paddingTop: "26px", left: "70px" }}>
-                                                    <Dropdown.Toggle as={CustomToggle} variant="Success" id="NotificationsBell" style={{ left: "70px" }} >
-                                                        {/* <span className="landingPageAccountText">{userState[0].name}</span> */}
+                                            <span>
+                                                <input type="text" placeholder="Search" id="searchInputSpanGrey" data-testid="searchbar" onChange={this.onSearch} onKeyDown={this.props.doSearchMethod} value={this.props.searchString} />
+                                            </span>
 
-                                                        <span className="accountDropDownGap"></span>
-                                                        < NotificationsBellSvg width={50} height={50} id="NotificationsBell" className={this.state.dropdownOpen ? "NotificationsBell" : null} style={{ cursor: 'pointer' }} />
+                                            {(this.props.searchString != '' && this.props.searchString != undefined) ?
+                                                <span className="searchInputClearGrey" data-testid="searchbar-clear-btn">
+                                                    <a style={{ cursor: 'pointer' }} href={'/search?search='} >
+                                                        <ClearButtonSvg />
+                                                    </a>
+                                                </span> : null}
+                                        </span>
+                                    </Col>
+                                    <Col >{
+                                        this.state.userState[0].loggedIn ?
+                                            <Dropdown ref={node => this.node = node} isOpen={this.state.dropdownOpen} onClick={this.toggle} style={{ paddingTop: "26px", left: "70px" }}>
+                                                <Dropdown.Toggle as={CustomToggle} variant="Success" id="NotificationsBell" style={{ left: "70px" }} >
+                                                    {/* <span className="landingPageAccountText">{userState[0].name}</span> */}
 
-                                                        <div >
-                                                            <NotificationBadge count={this.state.count} effect={Effect.SCALE} style={{ backgroundColor: '#29235c', top: '-50px', left: '44px', bottom: '', right: '' }} />
-                                                        </div>
-                                                    </Dropdown.Toggle>
-                                                    <Dropdown.Menu as={CustomMenu} style={{ overflowY: 'scroll', overflowX: "hidden", maxHeight: "432px", maxWidth: "400px" }}>
-                                                        <Messages userState={userState} />
-                                                    </Dropdown.Menu>
-                                                </Dropdown> : null
-                                        }
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </div>
-                        </Col>
-                        <Col xs={{ span: 6, order: 2 }} lg={{ span: 2, order: 3 }} style={{ backgroundColor: "", width: "10px" }}>
-                            <div style={{ backgroundColor: "", width: "" }} className="signLink">
-                                <UserMenu userState={userState} />
-                            </div>
-                        </Col>
-                    </Row>
+                                                    <span className="accountDropDownGap"></span>
+                                                    < NotificationsBellSvg width={50} height={50} id="NotificationsBell" className={this.state.dropdownOpen ? "NotificationsBell" : null} style={{ cursor: 'pointer' }} />
 
-                </div>
-            </>
+                                                    <div >
+                                                        <NotificationBadge count={this.state.count} effect={Effect.SCALE} style={{ backgroundColor: '#29235c', top: '-50px', left: '44px', bottom: '', right: '' }} />
+                                                    </div>
+                                                </Dropdown.Toggle>
+                                                <Dropdown.Menu as={CustomMenu} style={{ overflowY: 'scroll', overflowX: "hidden", maxHeight: "432px", maxWidth: "400px" }}>
+                                                    <Messages userState={userState} />
+                                                </Dropdown.Menu>
+                                            </Dropdown> : null
+                                    }
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </div>
+                    </Col>
+                    <Col xs={{ span: 6, order: 2 }} lg={{ span: 2, order: 3 }} style={{ backgroundColor: "", width: "10px" }}>
+                        <div style={{ backgroundColor: "", width: "" }} className="signLink">
+                            <UserMenu userState={userState} />
+                        </div>
+                    </Col>
+                </Row>
+
+            </div>
         );
     }
 }
