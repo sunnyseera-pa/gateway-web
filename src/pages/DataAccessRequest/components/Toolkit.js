06bb17f7afbe0eaec6bfaaaac4057cf91f3fadd4
Chris Marks <chris.marks@paconsulting.com>
2020-06-08 11:55:01 +0100

    Data access request, moved toolkit to own component, css class names updated, tidy dar file and update to searchbar test file to pass test

---
+++ src/pages/DataAccessRequest/components/Toolkit.js
@@ -0,0 +1,23 @@
+import React, {Fragment, useState} from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+function Toolkit() {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <Fragment>
+            <Button variant="white"  className="TechDetailButton" onClick={handleShow}>
+                View the toolkit
+            </Button>
+            <Modal show={show} onHide={handleClose} size="lg" aria-labelledby="contained-modal-title-vcenter" centered className="darModal" >
+                <iframe src="https://hda-toolkit.org/story_html5.html" className="darIframe"> </iframe>
+            </Modal>
+        </Fragment>
+    )
+}
+
+export default Toolkit;
+
