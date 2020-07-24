5f2599a416e269451348694ba0dce40c084dde7f
Ciara <ciara.ward@paconsulting.com>
2020-07-24 14:26:25 +0100

    IG-499 6 of the 7 KPIs working

--- src/pages/dashboard/AccountAnalyticsDashboard.js
+++ src/pages/dashboard/AccountAnalyticsDashboard.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import moment from 'moment';
 import UnmetDemand from './DARComponents/UnmetDemand';
 import { Row, Col, Button, Modal, Tabs, Tab, DropdownButton, Dropdown } from 'react-bootstrap';
+import DashboardKPI from './DARComponents/DashboardKPI';
 import Loading from '../commonComponents/Loading'
 import { Event, initGA } from '../../tracking';
 
@@ -15,15 +16,23 @@ class AccountAnalyticsDashboard extends React.Component {
         userState: [],
         key: 'Datasets',
         data: [],
+        statsDataType: [],
+        statsDataTime: [],
+        totalGAUsers: 0,
+        gaUsers: 0,
+        searchesWithResults: 0,
+        accessRequests: 0,
+        datasetsWithTechMetaData: 0,
         dates: getDatesForDropdown(),
         selectedOption: '',
-        isLoading: false
+        isLoading: true
     };
+    
 
     constructor(props) {
         super(props)
         this.state.userState = props.userState;
-        this.state.selectedOption = moment(this.state.dates[0]).format("MMMM YYYY");
+        this.state.selectedOption = this.state.dates[0];
     }
 
     handleSelect = (key) => {
@@ -33,14 +42,32 @@ class AccountAnalyticsDashboard extends React.Component {
             });
     }
 
-    handleDateSelect(eventKey, event) {
+    async handleDateSelect(eventKey, event) {
         this.setState({ selectedOption: this.state.dates[eventKey] });
         this.getUnmetDemand(this.state.dates[eventKey]);
+        await Promise.all([
+            this.getStats(),
+            this.getTotalGAUsers(),
+            this.getGAUsers(moment(this.state.dates[eventKey]).startOf('month').format("YYYY-MM-DD"), moment(this.state.dates[eventKey]).endOf('month').format("YYYY-MM-DD")),
+            this.getKPIs(this.state.dates[eventKey]),
+            this.getDatasetsWithTechMetadata()
+        ])
+        this.setState({isLoading: false})
       }
 
-    componentDidMount() {
+      async componentDidMount() {
         initGA('UA-166025838-1');
-        this.getUnmetDemand();
+        this.getUnmetDemand()
+
+        await Promise.all([
+            this.getStats(),
+            this.getTotalGAUsers(),
+            this.getGAUsers(moment(this.state.selectedOption).startOf('month').format("YYYY-MM-DD"), moment(this.state.selectedOption).endOf('month').format("YYYY-MM-DD")),
+            this.getKPIs(this.state.selectedOption),
+            this.getDatasetsWithTechMetadata()
+        ])
+        this.setState({isLoading: false})
+
     }
 
     getUnmetDemand(selectedOption){
@@ -57,12 +84,84 @@ class AccountAnalyticsDashboard extends React.Component {
         .then((res) => {
             this.setState({data: []});
             res.data.data.entity = this.state.key;
-            this.setState({ data: res.data.data, isLoading: false });
+            this.setState({ data: res.data.data}); 
         });
     }
 
+    getStats(){
+        return new Promise((resolve, reject) => {
+        axios.get(baseURL + '/api/v1/stats')
+        .then((res) => {
+            this.setState({
+                statsDataType: res.data.data.typecounts,
+                statsDataTime: res.data.data.daycounts
+            });
+            resolve();
+        });
+
+    });
+
+    }
+
+    getTotalGAUsers(){
+        return new Promise((resolve, reject) => {
+        axios.get(baseURL + '/api/v1/analyticsdashboard/totalusers')
+        .then((res) => {
+            this.setState({ totalGAUsers: res.data.data.rows[0][0] });
+            resolve();
+        });
+
+
+    });
+
+    }   
+
+    getGAUsers(startDate, endDate){
+        return new Promise((resolve, reject) => {
+        axios.get(baseURL + '/api/v1/analyticsdashboard/userspermonth?startDate=' + startDate + '&endDate=' + endDate)
+        .then((res) => {
+            this.setState({ gaUsers: res.data.data.rows[0][0] });
+            resolve();
+        });
+
+    });
+
+    } 
+
+    getKPIs(selectedDate){
+        return new Promise((resolve, reject) => {
+        axios.get(baseURL + '/api/v1/stats/kpis/' + selectedDate )
+        .then((res) => {
+            let haveResultsMonth = res.data.data.totalMonth - res.data.data.noResultsMonth;
+            let searchesWithResults = (haveResultsMonth / res.data.data.totalMonth) * 100;
+            this.setState({ searchesWithResults: searchesWithResults,
+                            accessRequests: res.data.data.accessRequestsMonth
+                         });
+            resolve();
+        }); 
+
+    });
+
+    }
+
+    getDatasetsWithTechMetadata(){
+        return new Promise((resolve, reject) => {
+        axios.get(baseURL + '/api/v1/stats/technicalmetadata')
+        .then((res) => {
+            let datasetsWithTechMetaData = (res.data.data.datasetsMetadata / res.data.data.totalDatasets) * 100;
+            this.setState({ datasetsWithTechMetaData: datasetsWithTechMetaData});
+            resolve();
+        }); 
+
+    });
+
+    }
+
     render() {
-        const { userState, key, isLoading, data, dates } = this.state;
+        const { userState, key, isLoading, data, dates, statsDataType, gaUsers, totalGAUsers, searchesWithResults, accessRequests, datasetsWithTechMetaData } = this.state;
+        const tempValue = "100";
+
+        let uniqueUsers = (statsDataType.person / totalGAUsers) * 100;
 
         if (isLoading) {
             return (
@@ -80,12 +179,11 @@ class AccountAnalyticsDashboard extends React.Component {
             <div>
                 <Row>
                     <Col sm={1} lg={1}></Col> 
-                    <Col sm={10} lg={10} className="dashboardPadding">
+                    <Col sm={10} lg={10} className="dashboardPadding">  
                         <Row className="accountHeader mt-4">
                             <Col sm={12} lg={12}>
                                 <Row >
                                     <Col sm={8} lg={8}>
-                                    {/* <Col sm={12} lg={12}> */}
                                         <span className="black-20">Dashboard</span>
                                     </Col>
                                     <Col sm={4} lg={4}>
@@ -108,6 +206,33 @@ class AccountAnalyticsDashboard extends React.Component {
                                         </div>
                                     </Col>
                                 </Row>
+                            </Col>
+                        </Row>
+
+                        <Row className="kpiContainer">
+                            <Col sm={3} lg={3} className="dashboardPadding"> 
+                                <DashboardKPI kpiText="total datasets" kpiValue={statsDataType.datasets}/>
+                            </Col>
+                            <Col sm={3} lg={3} className="dashboardPadding">
+                                <DashboardKPI kpiText="datasets with technical metadata" kpiValue={datasetsWithTechMetaData.toFixed(1)} percentageFlag={true}/>
+                            </Col>
+                            <Col sm={3} lg={3} className="dashboardPadding">
+                                <DashboardKPI kpiText="unique users this month" kpiValue={gaUsers}/>
+                            </Col>
+                            <Col sm={3} lg={3} className="dashboardPadding">                               
+                                <DashboardKPI kpiText="unique registered users" kpiValue={uniqueUsers.toFixed(1)} percentageFlag={true}/> 
+                            </Col>                        
+                        </Row>
+
+                        <Row className="kpiContainer">
+                            <Col sm={3} lg={3} className="dashboardPadding">
+                                <DashboardKPI kpiText="searches with results this month" kpiValue={searchesWithResults.toFixed(1)} percentageFlag={true}/>
+                            </Col>
+                            <Col sm={3} lg={3} className="dashboardPadding">
+                                <DashboardKPI kpiText="new access requests" kpiValue={accessRequests}/> 
+                            </Col>
+                            <Col sm={3} lg={3} className="dashboardPadding">
+                                <DashboardKPI kpiText="uptime this month" kpiValue={tempValue}/>
                             </Col>
                         </Row>
 
