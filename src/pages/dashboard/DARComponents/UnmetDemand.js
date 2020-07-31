1829746403b8c703e42ad1f1ca483e53a32bcb9f
Ciara <ciara.ward@paconsulting.com>
2020-07-31 15:58:55 +0100

    IG-499 added wrap for unmet demand search link

--- src/pages/dashboard/DARComponents/UnmetDemand.js
+++ src/pages/dashboard/DARComponents/UnmetDemand.js
@@ -71,8 +71,8 @@ class UnmetDemand extends React.Component {
                             </Col>
                             <Col sm={7} lg={7} className="gray800-14" style={{"float":"left", "padding-left":"0px"}}>
                                 <span style={{"float":"left", "padding-left":"0px"}}>
-                                    {!data || !data._id ? 'search term' : <a href={"/search?search="+data._id}> {data._id} </a>}
-                                </span>
+                                    {!data || !data._id ? 'search term' : <a href={"/search?search="+data._id} className="searchTermLink"> {data._id} </a>}
+                                </span>                
                             </Col>
                             <Col sm={2} lg={2} className="gray800-14">
                                 <span style={{"float":"left"}}>
