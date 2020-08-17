4538cc0f634fe7b431cf53062daba2dc7232d27d
Ciara <ciara.ward@paconsulting.com>
2020-08-17 15:13:56 +0100

    IG-437 Dashboard reviews tidyed

--- src/pages/dashboard/ReviewTools.js
+++ src/pages/dashboard/ReviewTools.js
@@ -110,13 +110,14 @@ class ReviewTools extends React.Component {
                             switch (key) {
                                 case "active":
                                     return (
-                                        <div>
+                                        <div> 
+                                            {activeCount <= 0 ? '' :
                                             <Row className="subHeader mt-3 gray800-14-bold">
                                                 <Col xs={2}>Added</Col>
                                                 <Col xs={5}>Name of reviewed</Col>
                                                 <Col xs={2}>Author of review</Col>
                                                 <Col xs={3}></Col>
-                                            </Row>
+                                            </Row>}
 
                                             {activeCount <= 0 ? 
                                             <Row className="margin-right-15">
@@ -149,12 +150,13 @@ class ReviewTools extends React.Component {
                                 case "pending":
                                     return (
                                         <div>
+                                            {reviewCount <= 0 ? '' :
                                             <Row className="subHeader mt-3 gray800-14-bold">
                                                 <Col xs={2}>Added</Col>
                                                 <Col xs={5}>Name of reviewed</Col>
                                                 <Col xs={2}>Author of review</Col>
                                                 <Col xs={3}></Col>
-                                            </Row>
+                                            </Row>}
 
                                             {reviewCount <= 0 ? 
                                             <Row className="margin-right-15">
@@ -193,12 +195,13 @@ class ReviewTools extends React.Component {
                                     case "rejected":
                                             return (
                                                 <div>
+                                                    {rejectedCount <= 0 ? '' :
                                                     <Row className="subHeader mt-3 gray800-14-bold">
                                                         <Col xs={2}>Added</Col>
                                                         <Col xs={5}>Name of reviewed</Col>
                                                         <Col xs={2}>Author of review</Col>
                                                         <Col xs={3}></Col>
-                                                    </Row>
+                                                    </Row>}
         
                                                     {rejectedCount <= 0 ? 
                                                     <Row className="margin-right-15">
@@ -230,12 +233,13 @@ class ReviewTools extends React.Component {
                                 case "archive":
                                     return (
                                         <div>
+                                            {archiveCount <= 0 ? '' :
                                             <Row className="subHeader mt-3 gray800-14-bold">
                                                 <Col xs={2}>Added</Col>
                                                 <Col xs={5}>Name of reviewed</Col>
                                                 <Col xs={2}>Author of review</Col>
                                                 <Col xs={3}></Col>
-                                            </Row>
+                                            </Row>}
 
                                             {archiveCount <= 0 ? 
                                                 <Row className="margin-right-15">
