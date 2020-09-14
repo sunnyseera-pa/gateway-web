36b5b5cde34585a50fd428d11fbbf30fde678e51
“Richard <Richard.Hobbs@PACONSULTING.COM>
2020-09-14 12:30:10 +0100

    Only displaying Results/Insights if there is data for it

--- src/pages/project/ProjectPage.js
+++ src/pages/project/ProjectPage.js
@@ -355,21 +355,22 @@ class ProjectDetail extends Component {
                           </div>
                         </Col>
                       </Row>
-
-                      <Row className="mt-2">
-                        <Col sm={12} lg={12}>
-                          <div className="rectangle">
-                            <Row className="gray800-14-bold">
-                              <Col sm={12}>Results/Insights</Col>
-                            </Row>
-                            <Row className="mt-3">
-                              <Col sm={12} className="gray800-14">
-                                <ReactMarkdown source={data.resultsInsights} />
-                              </Col>
-                            </Row>
-                          </div>
-                        </Col>
-                      </Row>
+                      
+                      {!_.isEmpty(data.resultsInsights) ? (
+                        <Row className="mt-2">
+                          <Col sm={12} lg={12}>
+                            <div className="rectangle">
+                              <Row className="gray800-14-bold">
+                                <Col sm={12}>Results/Insights</Col>
+                              </Row>
+                              <Row className="mt-3">
+                                <Col sm={12} className="gray800-14">
+                                  <ReactMarkdown source={data.resultsInsights} />
+                                </Col>
+                              </Row>
+                            </div>
+                          </Col>
+                        </Row>): ""}
 
                       <Row className="mt-2">
                         <Col sm={12}>
