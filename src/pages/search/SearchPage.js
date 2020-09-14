e52a8724b8ea5c423f54592105d6d9bc80a56a99
Paul McCafferty <Paul.McCafferty@PACONSULTING.COM>
2020-09-14 14:40:28 +0100

    Correction to login referrer code

--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -321,7 +321,7 @@ class SearchPage extends React.Component {
 
         if (this.state.userState[0].loggedIn === false ) {
             var values = queryString.parse(window.location.search);
-            if (values.showLogin === true && document.referrer === '') searchURL += '&loginReferrer=' + encodeURIComponent(document.referrer);
+            if (values.showLogin === true && document.referrer !== '') searchURL += '&loginReferrer=' + encodeURIComponent(document.referrer);
         }
 
         if (!skipHistory) { 
