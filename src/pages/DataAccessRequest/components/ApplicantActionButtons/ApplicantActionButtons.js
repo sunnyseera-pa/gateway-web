b57c5c3b1dc96d35340fa4b35e39bf973116f647
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2020-09-14 11:11:15 +0100

    Fixed applicant name output on email for short form

--- src/pages/DataAccessRequest/components/ApplicantActionButtons/ApplicantActionButtons.js
+++ src/pages/DataAccessRequest/components/ApplicantActionButtons/ApplicantActionButtons.js
@@ -4,7 +4,7 @@ import '../../DataAccessRequest.scss';
 const ApplicantActionButtons = ({allowedNavigation = false, onFormSubmit, onNextClick}) => {
   return (
     <Fragment>
-      <button className={`button-tertiary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onFormSubmit()}>Submit appliction</button>
+      <button className={`button-tertiary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onFormSubmit()}>Submit application</button>
       <button className={`button-primary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onNextClick()}>Next</button>
     </Fragment>
   );
