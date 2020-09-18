5fa2ee7205470be380ac3879b1be5a57b010c010
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2020-09-18 14:36:49 +0100

    Added notification for DAR removed link

--- src/pages/commonComponents/searchBar/SearchBar.js
+++ src/pages/commonComponents/searchBar/SearchBar.js
@@ -345,6 +345,20 @@ class SearchBar extends React.Component {
                                                                     </Fragment>
                                                                 )
                                                             }
+                                                            else if(dat.messageType === 'data access request unlinked'){
+                                                                return (
+                                                                    <Fragment key={`message-${index}`}>
+                                                                        <Row className={dat.isRead === 'true' || clearMessage ? "notificationReadBackground" : ''}>
+                                                                                <Col xs={10}>
+                                                                                    <div className="notificationDate">{messageDateString + '\n'}</div>
+                                                                                    <div className="notificationInfoHolder"><span class="notificationInfo">{dat.messageDescription}</span></div> 
+                                                                                </Col>
+                                                                                <Col xs={2}>{dat.isRead === 'false' && !clearMessage ? <SVGIcon name="newnotificationicon" width={20} height={20} visble='true' style={{ float: "right", fill: "#3db28c", paddingRight: "0px", marginRight: "10px", marginTop: "5px" }} fill={"#3db28c"} stroke='none' /> : null}</Col>
+                                                                            </Row>
+                                                                            <Dropdown.Divider style={{ margin: "0px" }} />
+                                                                    </Fragment>
+                                                                )
+                                                            }
                                                             else if(dat.messageType === 'added collection'){
                                                                 return(
                                                                     <Fragment key={`message-${index}`}>
