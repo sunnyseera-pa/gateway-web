0ec67d4ad75f2993adab0ecc718de6e5357506e4
Robin Kavanagh <robin.kavanagh@paconsulting.com>
2020-09-23 17:40:11 +0100

    Completed UI modifications for adding/removing collaborators

--- src/pages/DataAccessRequest/components/ApplicantActionButtons/ApplicantActionButtons.js
+++ src/pages/DataAccessRequest/components/ApplicantActionButtons/ApplicantActionButtons.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import '../../DataAccessRequest.scss'; 
 
-const ApplicantActionButtons = ({allowedNavigation = false, onFormSubmit, onNextClick}) => {
+const ApplicantActionButtons = ({allowedNavigation = false, onFormSubmit, onNextClick, onShowContributorModal}) => {
   return (
     <Fragment>
       <button className={`button-tertiary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onFormSubmit()}>Submit application</button>
+      <button className={`button-tertiary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onShowContributorModal()}>Contributors</button>
       <button className={`button-primary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onNextClick()}>Next</button>
     </Fragment>
   );
