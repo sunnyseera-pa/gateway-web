70b5b9a75dbfa7eb94f79050fe4848346133cd09
Chris Marks <chris.marks@paconsulting.com>
2021-01-15 15:29:15 +0000

    Test for GeneralHelper util

--- src/utils/__tests__/GeneralHelper.test.js
+++ src/utils/__tests__/GeneralHelper.test.js
@@ -1,7 +1,17 @@
-import GeneralHelper from '../GeneralHelper.util';
+import {isEditMode} from '../GeneralHelper.util';
 
 describe('Test GeneralHelper Utility', () => {
-  it('should test isEditMode', () => {
-   expect(1).toEqual(1);
+  it('should test isEditMode fn valid url', () => {
+    let validSrc = '/projects/edit/456765';
+    let isEdit= isEditMode(validSrc);
+
+    expect(isEdit).toBe(true);
+  });
+
+  it('should test isEditMode fn inValid url', () => {
+    let invalidSrc = '/projects/465465';
+    let isEdit= isEditMode(invalidSrc);
+
+    expect(isEdit).toBe(false);
   });
 });
