4b9c2ed82824e99aebc47b89f70482a9f704d67f
CiaraWardPA <ciara.ward@paconsulting.com>
2021-01-22 16:29:44 +0000

    IG-1246 setState moved into one

--- src/pages/course/AddEditCoursePage.js
+++ src/pages/course/AddEditCoursePage.js
@@ -200,8 +200,7 @@ class AddEditCoursePage extends React.Component {
 			return newRelatedObject;
 		});
 
-		this.setState({ relatedObjects: [...relatedObjects, ...newRelatedObjects] });
-		this.setState({ tempRelatedObjectIds: [] });
+		this.setState({ relatedObjects: [...relatedObjects, ...newRelatedObjects], tempRelatedObjectIds: [] });
 	};
 
 	clearRelatedObjects = () => {
