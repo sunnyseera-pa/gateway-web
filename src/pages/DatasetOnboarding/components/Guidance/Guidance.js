5c502b994b486e7915e825582c0e2a13577fa856
Paul McCafferty <>
2021-02-26 09:15:15 +0000

    Adding guidance icon to the guidance question

--- src/pages/DatasetOnboarding/components/Guidance/Guidance.js
+++ src/pages/DatasetOnboarding/components/Guidance/Guidance.js
@@ -2,10 +2,10 @@ import React, { Fragment } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { ReactComponent as CloseButtonSvg } from '../../../../images/close-alt.svg';
 
-const Guidance = ({activeGuidance, resetGuidance}) => {
+const Guidance = ({ activeGuidance, resetGuidance }) => {
 	return (
-    <Fragment>
-    {activeGuidance ? (
+		<Fragment>
+			{activeGuidance ? (
 				<Fragment>
 					<header>
 						<div>
@@ -15,13 +15,18 @@ const Guidance = ({activeGuidance, resetGuidance}) => {
 						<CloseButtonSvg width='16px' height='16px' fill='#475da' onClick={resetGuidance} />
 					</header>
 					<main className='gray800-14'>
-                        <ReactMarkdown source={activeGuidance} />
+						<ReactMarkdown source={activeGuidance} />
 					</main>
 				</Fragment>
 			) : (
-				<div className='darTab-guidance'>Click on a question guidance to view details</div>
+				<div className='darTab-guidance'>
+					Click on a question guidance to view details&nbsp;
+					<span className='purple-bold-16'>
+						<i className='far fa-question-circle mr-2' />
+					</span>
+				</div>
 			)}
-    </Fragment>
+		</Fragment>
 	);
 };
 
