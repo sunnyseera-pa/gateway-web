27589d43d6d65a835898f06455ee5fedd4ecb7aa
Paul McCafferty <>
2021-03-28 15:21:59 +0100

    Updating dataset page to show datasets when qualified relation contains all, also hide any relations if length is over 10

--- src/pages/dataset/DatasetPage.js
+++ src/pages/dataset/DatasetPage.js
@@ -88,6 +88,7 @@ class DatasetDetail extends Component {
 		allowNewMessage: false,
 		dataRequestModalContent: {},
 		showAllPhenotype: false,
+		showAllLinkedDatasets: false,
 		showEmpty: false,
 		emptyFlagDetails: false,
 		emptyFlagCoverage: false,
@@ -324,15 +325,17 @@ class DatasetDetail extends Component {
 			});
 		} else {
 			// 2. Check if relation is a String that matches a dataset title
-			await axios.get(baseURL + '/api/v1/relatedobject/linkeddatasets/' + relation).then(async res => {
+			await axios.get(baseURL + '/api/v1/relatedobject/linkeddatasets/' + encodeURIComponent(relation)).then(async res => {
 				const { datasetFound, pid, name, publisher } = res.data;
 				if (datasetFound && !_.isNil(pid)) {
-					linkedDatasets.unshift({
-						title: name,
-						info: publisher,
-						type: 'gatewaylink',
-						id: pid,
-					});
+					if (pid !== this.state.data.pid) {
+						linkedDatasets.push({
+							title: name,
+							info: publisher,
+							type: 'gatewaylink',
+							id: pid,
+						});
+					}
 				} else {
 					// 3. Else determine that relation is an unrecognised dataset title
 					linkedDatasets.push({
@@ -584,6 +587,10 @@ class DatasetDetail extends Component {
 		this.setState({ showAllPhenotype: true });
 	};
 
+	showAllLinkedDatasets = () => {
+		this.setState({ showAllLinkedDatasets: true });
+	};
+
 	render() {
 		const {
 			searchString,
@@ -601,6 +608,7 @@ class DatasetDetail extends Component {
 			requiresModal,
 			allowsMessaging,
 			showAllPhenotype,
+			showAllLinkedDatasets,
 			collections,
 			emptyFlagDetails,
 			emptyFlagCoverage,
@@ -1278,70 +1286,153 @@ class DatasetDetail extends Component {
 														</Col>
 													</Row>
 
-													{linkedDatasets.map(relation => (
-														<Row className='pixelGapTop'>
-															<Col sm={12} m={12}>
-																<div className='rectangle'>
-																	<Row className='gray800-14-bold'>
-																		<Col sm={9} m={9} lg={9}>
-																			<Row>
-																				<Col sm={1} m={1} lg={1}>
-																					<SVGIcon
-																						name={
-																							relation.type === 'gatewaylink'
-																								? 'dataseticon'
-																								: relation.type === 'externallink'
-																								? 'externallink'
-																								: 'searchicon'
-																						}
-																						fill={'#475da7'}
-																						className='svg-16 mr-2'
-																						viewBox='-2 -2 20 20'
-																					/>
-																				</Col>
-																				<Col sm={11} m={11} lg={11} className='datasetLinked'>
-																					{relation.type === 'gatewaylink' ? (
-																						<span>
-																							<a
-																								href={'/dataset/' + relation.id}
+													{!showAllLinkedDatasets
+														? linkedDatasets.slice(0, 10).map(relation => {
+																return (
+																	<Row className='pixelGapTop'>
+																		<Col sm={12} m={12}>
+																			<div className='rectangle'>
+																				<Row className='gray800-14-bold'>
+																					<Col sm={9} m={9} lg={9}>
+																						<Row>
+																							<Col sm={1} m={1} lg={1}>
+																								<SVGIcon
+																									name={
+																										relation.type === 'gatewaylink'
+																											? 'dataseticon'
+																											: relation.type === 'externallink'
+																											? 'externallink'
+																											: 'searchicon'
+																									}
+																									fill={'#475da7'}
+																									className='svg-16 mr-2'
+																									viewBox='-2 -2 20 20'
+																								/>
+																							</Col>
+																							<Col sm={11} m={11} lg={11} className='datasetLinked'>
+																								{relation.type === 'gatewaylink' ? (
+																									<span>
+																										<a
+																											href={'/dataset/' + relation.id}
+																											target='_blank'
+																											rel='noopener noreferrer'
+																											className='gray800-14-bold pointer overflowWrap'>
+																											{relation.title}
+																										</a>
+																									</span>
+																								) : relation.type === 'externallink' ? (
+																									<Linkify componentDecorator={componentDecorator}>{relation.title}</Linkify>
+																								) : (
+																									<span className='gray800-14-bold overflowWrap'>{relation.title}</span>
+																								)}
+																							</Col>
+																						</Row>
+																						<Row>
+																							<Col sm={1} m={1} lg={1} />
+																							<Col sm={11} m={11} lg={11} className='datasetLinked'>
+																								<span className='gray800-14'>{relation.info}</span>
+																							</Col>
+																						</Row>
+																					</Col>
+																					<Col sm={3} m={3} lg={3}>
+																						{relation.type === 'text' ? (
+																							<Button
+																								variant='white'
+																								href={'/search?search=' + relation.title}
 																								target='_blank'
-																								rel='noopener noreferrer'
-																								className='gray800-14-bold pointer overflowWrap'>
-																								{relation.title}
-																							</a>
-																						</span>
-																					) : relation.type === 'externallink' ? (
-																						<Linkify componentDecorator={componentDecorator}>{relation.title}</Linkify>
-																					) : (
-																						<span className='gray800-14-bold overflowWrap'>{relation.title}</span>
-																					)}
-																				</Col>
-																			</Row>
-																			<Row>
-																				<Col sm={1} m={1} lg={1} />
-																				<Col sm={11} m={11} lg={11} className='datasetLinked'>
-																					<span className='gray800-14'>{relation.info}</span>
-																				</Col>
-																			</Row>
-																		</Col>
-																		<Col sm={3} m={3} lg={3}>
-																			{relation.type === 'text' ? (
-																				<Button
-																					variant='white'
-																					href={'/search?search=' + relation.title}
-																					target='_blank'
-																					className='gatewaySearchButton floatRightLinkedDataset'>
-																					Search on gateway
-																				</Button>
-																			) : (
-																				''
-																			)}
+																								className='gatewaySearchButton floatRightLinkedDataset'>
+																								Search on gateway
+																							</Button>
+																						) : (
+																							''
+																						)}
+																					</Col>
+																				</Row>
+																			</div>
 																		</Col>
 																	</Row>
+																);
+														  })
+														: linkedDatasets.map(relation => {
+																return (
+																	<Row className='pixelGapTop'>
+																		<Col sm={12} m={12}>
+																			<div className='rectangle'>
+																				<Row className='gray800-14-bold'>
+																					<Col sm={9} m={9} lg={9}>
+																						<Row>
+																							<Col sm={1} m={1} lg={1}>
+																								<SVGIcon
+																									name={
+																										relation.type === 'gatewaylink'
+																											? 'dataseticon'
+																											: relation.type === 'externallink'
+																											? 'externallink'
+																											: 'searchicon'
+																									}
+																									fill={'#475da7'}
+																									className='svg-16 mr-2'
+																									viewBox='-2 -2 20 20'
+																								/>
+																							</Col>
+																							<Col sm={11} m={11} lg={11} className='datasetLinked'>
+																								{relation.type === 'gatewaylink' ? (
+																									<span>
+																										<a
+																											href={'/dataset/' + relation.id}
+																											target='_blank'
+																											rel='noopener noreferrer'
+																											className='gray800-14-bold pointer overflowWrap'>
+																											{relation.title}
+																										</a>
+																									</span>
+																								) : relation.type === 'externallink' ? (
+																									<Linkify componentDecorator={componentDecorator}>{relation.title}</Linkify>
+																								) : (
+																									<span className='gray800-14-bold overflowWrap'>{relation.title}</span>
+																								)}
+																							</Col>
+																						</Row>
+																						<Row>
+																							<Col sm={1} m={1} lg={1} />
+																							<Col sm={11} m={11} lg={11} className='datasetLinked'>
+																								<span className='gray800-14'>{relation.info}</span>
+																							</Col>
+																						</Row>
+																					</Col>
+																					<Col sm={3} m={3} lg={3}>
+																						{relation.type === 'text' ? (
+																							<Button
+																								variant='white'
+																								href={'/search?search=' + relation.title}
+																								target='_blank'
+																								className='gatewaySearchButton floatRightLinkedDataset'>
+																								Search on gateway
+																							</Button>
+																						) : (
+																							''
+																						)}
+																					</Col>
+																				</Row>
+																			</div>
+																		</Col>
+																	</Row>
+																);
+														  })}
+
+													{!showAllLinkedDatasets && linkedDatasets.length > 10 ? (
+														<Row className='pixelGapTop text-center'>
+															<Col sm={12} className='purple-14'>
+																<div className='rectangle'>
+																	<span onClick={() => this.showAllLinkedDatasets()} style={{ cursor: 'pointer' }}>
+																		Show all linked datasets
+																	</span>
 																</div>
 															</Col>
 														</Row>
-													))}
+													) : (
+														''
+													)}
 												</Fragment>
 											) : (
 												''
