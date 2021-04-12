320e307675009277898f4a24a12fee5846c815aa
Chris Marks <chris.marks@paconsulting.com>
2021-04-12 11:36:35 +0100

    Check for invalid email errors before submission

--- src/pages/dashboard/Team/TeamEmailAlertModal.js
+++ src/pages/dashboard/Team/TeamEmailAlertModal.js
@@ -3,15 +3,15 @@ import { Modal } from 'react-bootstrap';
 import { ReactComponent as CloseButtonSvg } from '../../../images/close-alt.svg';
 import './TeamEmailAlertModal.scss';
 
-const TeamEmailAlertModal = ({ open, close }) => {
+const TeamEmailAlertModal = ({ open, close, options }) => {
 	return (
 		<Fragment>
 			<Modal show={open} onHide={close} aria-labelledby='contained-modal-title-vcenter' centered className='teamEmailAlertModal'>
 				<div className='teamEmailAlertModal-header'>
-					<h1 className='black-20-semibold'>You must have one email address selected</h1>
+					<h1 className='black-20-semibold'>{options.title}</h1>
 					<CloseButtonSvg className='teamEmailAlertModal-header--close' onClick={close} />
 				</div>
-				<div className='teamEmailAlertModal-body'>At least one email address is needed to receive notifications from the gateway.</div>
+				<div className='teamEmailAlertModal-body'>{options.body}</div>
 			</Modal>
 		</Fragment>
 	);
