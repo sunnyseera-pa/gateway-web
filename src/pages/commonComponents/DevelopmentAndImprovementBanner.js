26da39b082e7f8d3eb90db56ee1281fe6f750a3d
Richard <richard.hobbs@paconsulting.com>
2021-04-20 09:25:31 +0100

    Changed data-test-id

--- src/pages/commonComponents/DevelopmentAndImprovementBanner.js
+++ src/pages/commonComponents/DevelopmentAndImprovementBanner.js
@@ -7,7 +7,8 @@ const DevelopmentAndImprovementBanner = () => {
 		<Alert.Link
 			target='_blank'
 			className='dev-and-improv-banner'
-			href='https://discourse.healthdatagateway.org/t/about-the-development-and-improvement-group/498'>
+			href='https://discourse.healthdatagateway.org/t/about-the-development-and-improvement-group/498'
+			data-test-id='dev-and-improvement'>
 			Want to get involved in shaping the Gateway? Join our development and improvement group.
 		</Alert.Link>
 	);
