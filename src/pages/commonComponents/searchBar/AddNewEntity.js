b4f7409eb71a885257fd2f49476ef6ea59f04fe2
Richard <richard.hobbs@paconsulting.com>
2021-06-07 15:59:33 +0100

    Showing login modal when a logged out user clicks Add new entity button

--- src/pages/commonComponents/searchBar/AddNewEntity.js
+++ src/pages/commonComponents/searchBar/AddNewEntity.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Dropdown } from 'react-bootstrap';
 import { ReactComponent as ChevronBottom } from '../../../images/chevron-bottom.svg';
 import './AddNewEntity.scss';
@@ -15,46 +15,58 @@ const CustomMenu = React.forwardRef(({ children, style, className, 'aria-labelle
 	);
 });
 
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+const CustomToggle = React.forwardRef(({ children, onClick, loggedIn, showLoginModal }, ref) => (
 	<button
 		href=''
 		ref={ref}
 		onClick={e => {
 			e.preventDefault();
-			onClick(e);
+			if (loggedIn) {
+				onClick(e);
+			} else {
+				showLoginModal();
+			}
 		}}
 		className='addNewEntityDropdown'>
 		{children}
 	</button>
 ));
 
-const AddNewEntity = () => {
-	return (
-		<Dropdown data-test-id='addNewEntityDropdown' className='addNewEntityDropdown'>
-			<Dropdown.Toggle as={CustomToggle}>
-				+ Add new
-				<span className='addNewDropDownGap'></span>
-				<ChevronBottom />
-			</Dropdown.Toggle>
+const AddNewEntity = props => {
+	const showLoginModal = () => {
+		let modalID = 'myModal';
+		document.getElementById(modalID).style.display = 'block';
+		document.getElementById('modalRequestSection').style.display = 'none';
+	};
 
-			<Dropdown.Menu as={CustomMenu} className='addNewEntityMenu'>
-				<Dropdown.Item href='/collection/add' className='black-14' data-test-id='addNewCollection'>
-					Collection
-				</Dropdown.Item>
-				<Dropdown.Item href='/course/add' className='black-14' data-test-id='addNewCourse'>
-					Course
-				</Dropdown.Item>
-				<Dropdown.Item href='/paper/add' className='black-14' data-test-id='addNewPaper'>
-					Paper
-				</Dropdown.Item>
-				<Dropdown.Item href='/project/add' className='black-14' data-test-id='addNewProject'>
-					Project
-				</Dropdown.Item>
-				<Dropdown.Item href='/tool/add' className='black-14 ' data-test-id='addNewTool'>
-					Tool
-				</Dropdown.Item>
-			</Dropdown.Menu>
-		</Dropdown>
+	return (
+		<Fragment>
+			<Dropdown data-test-id='addNewEntityDropdown' className='addNewEntityDropdown'>
+				<Dropdown.Toggle as={CustomToggle} showLoginModal={showLoginModal} loggedIn={props.loggedIn}>
+					+ Add new
+					<span className='addNewDropDownGap'></span>
+					<ChevronBottom />
+				</Dropdown.Toggle>
+
+				<Dropdown.Menu as={CustomMenu} className='addNewEntityMenu'>
+					<Dropdown.Item href='/collection/add' className='black-14' data-test-id='addNewCollection'>
+						Collection
+					</Dropdown.Item>
+					<Dropdown.Item href='/course/add' className='black-14' data-test-id='addNewCourse'>
+						Course
+					</Dropdown.Item>
+					<Dropdown.Item href='/paper/add' className='black-14' data-test-id='addNewPaper'>
+						Paper
+					</Dropdown.Item>
+					<Dropdown.Item href='/project/add' className='black-14' data-test-id='addNewProject'>
+						Project
+					</Dropdown.Item>
+					<Dropdown.Item href='/tool/add' className='black-14 ' data-test-id='addNewTool'>
+						Tool
+					</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown>
+		</Fragment>
 	);
 };
 
