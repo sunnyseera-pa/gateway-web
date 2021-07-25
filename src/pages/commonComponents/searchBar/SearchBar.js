ee75558eaa68123ddf81488dabbc54a7b0c4d58b
Paul McCafferty <>
2021-07-25 17:11:18 +0100

    Updates

--- src/pages/commonComponents/searchBar/SearchBar.js
+++ src/pages/commonComponents/searchBar/SearchBar.js
@@ -630,6 +630,41 @@ class SearchBar extends React.Component {
 																					<Dropdown.Divider style={{ margin: '0px' }} />
 																				</Fragment>
 																			);
+																		} else if (dat.messageType === 'data access message sent') {
+																			return (
+																				<Fragment key={`message-${index}`}>
+																					<Row className={dat.isRead === 'true' || clearMessage ? 'notificationReadBackground' : ''}>
+																						<Col xs={10}>
+																							<div className='notificationDate'>{messageDateString + '\n'}</div>
+																							<div className='notificationInfoHolder'>
+																								<a href={`/data-access-request/${dat.messageDataRequestID}`} class='notificationInfo'>
+																									{dat.messageDescription}
+																								</a>
+																							</div>
+																						</Col>
+																						<Col xs={2}>
+																							{dat.isRead === 'false' && !clearMessage ? (
+																								<SVGIcon
+																									name='newnotificationicon'
+																									width={20}
+																									height={20}
+																									visble='true'
+																									style={{
+																										float: 'right',
+																										fill: '#3db28c',
+																										paddingRight: '0px',
+																										marginRight: '10px',
+																										marginTop: '5px',
+																									}}
+																									fill={'#3db28c'}
+																									stroke='none'
+																								/>
+																							) : null}
+																						</Col>
+																					</Row>
+																					<Dropdown.Divider style={{ margin: '0px' }} />
+																				</Fragment>
+																			);
 																		} else if (dat.messageType === 'team unlinked') {
 																			return (
 																				<Fragment key={`message-${index}`}>
