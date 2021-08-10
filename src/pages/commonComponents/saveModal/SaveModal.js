20fde95378f3a274fe41061c36c9875e0d7e65ea
umma-pa <umma.gohil@paconsulting.com>
2021-08-10 10:43:22 +0100

    BE integration set up

--- src/pages/commonComponents/saveModal/SaveModal.js
+++ src/pages/commonComponents/saveModal/SaveModal.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Modal, Button } from 'react-bootstrap';
 import './SaveModal.scss';
 
 const SaveModal = ({ show, onHide }) => {
+	const [title, setTitle] = useState('');
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(false);
+
+	const handleSubmit = () => {
+		const data = {
+			title: title,
+		};
+		axios
+			.post('https://link-here.com', data)
+			.then(res => {
+				setData(res.data);
+				setTitle('');
+			})
+			.catch(err => {
+				setError(true);
+			});
+	};
+
 	return (
 		<Modal show={show} onHide={onHide} className='save-modal'>
 			<Modal.Header closeButton>
@@ -12,13 +31,15 @@ const SaveModal = ({ show, onHide }) => {
 			<Modal.Body>
 				<p className='black-14'>Are you sure you want to save this search preference? If yes, please provide a title for this search.</p>
 				<label className='black-14'>Title</label>
-				<input type='text' className='save-modal-input' />
+				<input type='text' className='save-modal-input' value={title} onChange={e => setTitle(e.target.value)} />
 			</Modal.Body>
 			<Modal.Footer className='saved-modal-footer'>
 				<Button variant='outline-primary saved-no' onClick={onHide}>
 					No, nevermind
 				</Button>
-				<Button>Save</Button>
+				<Button type='submit' onClick={handleSubmit}>
+					Save
+				</Button>
 			</Modal.Footer>
 		</Modal>
 	);
