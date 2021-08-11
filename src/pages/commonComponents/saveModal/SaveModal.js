8d709725a7b95dbb3fb3a91a9ce4840c114da8ee
umma-pa <umma.gohil@paconsulting.com>
2021-08-11 16:22:54 +0100

    Formik added to save modal

--- src/pages/commonComponents/saveModal/SaveModal.js
+++ src/pages/commonComponents/saveModal/SaveModal.js
@@ -34,6 +34,32 @@ const SaveModal = ({ show, onHide, showAlert }) => {
 			});
 	};
 
+	const formik = useFormik({
+		initialValues: {
+			title: '',
+		},
+
+		validationSchema: Yup.object({
+			title: Yup.string().required('This cannot be empty'),
+		}),
+
+		onSubmit: values => {
+			axios
+				.post(baseURL + '/api/v1/search-preferences', { data })
+				.then(res => {
+					setData(res.data);
+					setTitle('');
+					console.log('works');
+					setClose(onHide);
+					setSavedAlert(!showAlert);
+				})
+				.catch(err => {
+					setError(true);
+					console.log(err);
+				});
+		},
+	});
+
 	return (
 		<Modal show={show} onHide={onHide} className='save-modal'>
 			<Modal.Header closeButton>
@@ -43,12 +69,13 @@ const SaveModal = ({ show, onHide, showAlert }) => {
 				<p className='black-14'>Are you sure you want to save this search preference? If yes, please provide a title for this search.</p>
 				<label className='black-14'>Title</label>
 				<input type='text' className='save-modal-input' value={title} onChange={e => setTitle(e.target.value)} required />
+				{formik.touched.title && formik.errors.name ? <div className='errorMessages'>{formik.errors.name}</div> : null}
 			</Modal.Body>
 			<Modal.Footer className='saved-modal-footer'>
 				<Button variant='outline-primary saved-no' onClick={onHide}>
 					No, nevermind
 				</Button>
-				<Button type='submit' onClick={handleSubmit}>
+				<Button type='submit' save onClick={formik.handleSubmit}>
 					Save
 				</Button>
 			</Modal.Footer>
