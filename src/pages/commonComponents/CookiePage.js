ebd10d9435c0548b98ac9980ea0e9a6d24e2f837
Dearbhail Rice <dearbhail.rice@paconsulting.com>
2021-08-13 14:25:43 +0100

    ICODARA-109 base page created for cookie list and linked to footer

---
+++ src/pages/commonComponents/CookiePage.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
+import Iframe from 'react-iframe';
+import './CookiePage.scss';
+import { Container } from 'react-bootstrap';
+import SearchBar from '../commonComponents/searchBar/SearchBar';
+import Loading from '../commonComponents/Loading';
+import SideDrawer from '../commonComponents/sidedrawer/SideDrawer';
+import UserMessages from '../commonComponents/userMessages/UserMessages';
+import DataSetModal from '../commonComponents/dataSetModal/DataSetModal';
+
+var baseURL = require('../commonComponents/BaseURL').getURL();
+
+class CookiePage extends React.Component {
+	state = {
+		isAvailable: true,
+		userState: [],
+		isLoading: true,
+		showDrawer: false,
+		showModal: false,
+		context: {},
+	};
+
+	constructor(props) {
+		super(props);
+		this.state.userState = props.userState;
+		this.searchBar = React.createRef();
+	}
+
+	async componentDidMount() {
+		axios
+			.head('', { withCredentials: false })
+			.then(res => {
+				this.setState({
+					isAvailable: true,
+					isLoading: false,
+				});
+			})
+			.catch(error => {
+				this.setState({
+					isAvailable: false,
+				});
+				console.error(error.message);
+			});
+	}
+	toggleDrawer = () => {
+		this.setState(prevState => {
+			if (prevState.showDrawer === true) {
+				this.searchBar.current.getNumberOfUnreadMessages();
+			}
+			return { showDrawer: !prevState.showDrawer };
+		});
+	};
+
+	toggleModal = (showEnquiry = false, context = {}) => {
+		this.setState(prevState => {
+			return { showModal: !prevState.showModal, context, showDrawer: showEnquiry };
+		});
+	};
+	render() {
+		const { isAvailable, userState, showDrawer, showModal, context, isLoading } = this.state;
+		if (isLoading) {
+			return (
+				<Container>
+					<Loading />
+				</Container>
+			);
+		}
+		return (
+			<div className='searchTabsHolder'>
+				<SearchBar
+					ref={this.searchBar}
+					doSearchMethod={this.doSearch}
+					doUpdateSearchString={this.updateSearchString}
+					doToggleDrawer={this.toggleDrawer}
+					userState={userState}
+				/>
+				<div className='cookie-page-header'>
+					<div className='cookie-page-header-title'>ICODA Gateway</div>
+				</div>
+				<div className='cookie-page-button text-center'>
+					<Button
+						className='addButton'
+						onClick={e => {
+							window.location.href = `/search?search=&tab=Collections`;
+						}}>
+						Search for datasets
+					</Button>
+				</div>
+				<div className='collection-rectangle cookie-page-body'>
+					<div className='pad-left-24 pad-right-24 pad-top-24 pad-bottom-16 col-lg-12 col-sm-12' />
+					<Iframe
+						url='https://icoda-research.org/icoda-gateway/'
+						width='100%'
+						height='10px'
+						id='aboutIframe'
+						frameBorder='0'
+						display='initial'
+						position='relative'
+					/>
+				</div>
+
+				<SideDrawer open={showDrawer} closed={this.toggleDrawer}>
+					<UserMessages
+						userState={userState[0]}
+						closed={this.toggleDrawer}
+						toggleModal={this.toggleModal}
+						drawerIsOpen={this.state.showDrawer}
+					/>
+				</SideDrawer>
+
+				<DataSetModal open={showModal} context={context} closed={this.toggleModal} userState={userState[0]} />
+			</div>
+		);
+	}
+}
+
+export default CookiePage;
