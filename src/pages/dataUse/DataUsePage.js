2149bcf48da60f2281edc39d9a1a2d29c764d1a8
umma-pa <umma.gohil@paconsulting.com>
2021-09-01 17:01:13 +0100

    Title + upload button

--- src/pages/dataUse/DataUsePage.js
+++ src/pages/dataUse/DataUsePage.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import './DataUse.scss';
-//import SearchBar from '../commonComponents/searchBar/SearchBar';
 
 const DataUsePage = () => (
-	<>
-		DataUse
-		{/* 
-  <SearchBar
-    ref={this.searchBar}
-    searchString={searchString}
-    doSearchMethod={this.doSearch}
-    doUpdateSearchString={this.updateSearchString}
-    userState={userState}
-    doToggleDrawer={this.toggleDrawer}
-  />
-  */}
-	</>
+	<Container>
+		<Row className='datause-card'>
+			<Col md={10}>
+				<Row>
+					<p className='black-20-semibold'>Data uses</p>
+				</Row>
+				<Row>
+					<p className='soft-black-14 datause-para'>Manage your data use register by uploading or editing data uses.</p>
+				</Row>
+			</Col>
+			<Col md={2} className='datause-button-grid'>
+				<Button className='datause-button'>+ Upload data uses</Button>
+			</Col>
+		</Row>
+		<Row className='datause-tabs'>Tabs</Row>
+	</Container>
 );
-
 export default DataUsePage;
