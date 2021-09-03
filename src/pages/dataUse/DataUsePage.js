086a0272b84bd3c0bec4e4a265f777f8cd821a76
umma-pa <umma.gohil@paconsulting.com>
2021-09-03 11:13:39 +0100

    Data in correct tabs

--- src/pages/dataUse/DataUsePage.js
+++ src/pages/dataUse/DataUsePage.js
@@ -5,8 +5,27 @@ import Table from './DataUseTable';
 import './DataUse.scss';
 
 //filter the data at this stage and then use it as props for each tab?
+
+// map over the pending, active etc. data here and then send that data down via a prop
+
+// for now have multiple tabs
+
+// and then think about mapping over the tabs AND the status
 const DataUsePage = () => {
 	const tabs = ['Active', 'Pending approval', 'Rejected', 'Archived'];
+
+	const active = Data.filter(a => a.status === 'active');
+	console.log(active);
+
+	const pending = Data.filter(a => a.status === 'pending approval');
+	console.log(pending);
+
+	const rejected = Data.filter(a => a.status === 'rejected');
+	console.log(rejected);
+
+	const archived = Data.filter(a => a.status === 'archived');
+	console.log(archived);
+
 	return (
 		<Container>
 			<Row className='datause-card'>
@@ -26,12 +45,14 @@ const DataUsePage = () => {
 				<Tabs defaultActiveKey={'Active'} className='gray700-13'>
 					{tabs.map(tabName => (
 						<Tab eventKey={tabName} title={tabName}>
-							Data
+							{tabName === 'Active' && <Table data={active} />}
+							{tabName === 'Pending approval' && <Table data={pending} />}
+							{tabName === 'Rejected' && <Table data={rejected} />}
+							{tabName === 'Archived' && <Table data={archived} />}
 						</Tab>
 					))}
 				</Tabs>
 			</Row>
-			<Table data={Data} />
 		</Container>
 	);
 };
