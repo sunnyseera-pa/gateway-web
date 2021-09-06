81592f4d3647c45477f55b6aff64eb9a20949231
umma-pa <umma.gohil@paconsulting.com>
2021-09-06 17:13:31 +0100

    Pagination

--- src/pages/dataUse/DataUsePage.js
+++ src/pages/dataUse/DataUsePage.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button, Tab, Tabs } from 'react-bootstrap';
 import Data from './MockData.json';
 import Table from './DataUseTable';
+import Pagination from './DataUsePagination';
 import './DataUse.scss';
 
 const DataUsePage = () => {
+	const [row, setRow] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [rowsPerPage, setRowsPerPage] = useState(2);
+
+	useEffect(() => {
+		setRow(Data);
+	}, []);
+
+	const indexOfLastRow = currentPage * rowsPerPage;
+	const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+
 	const tabs = ['Active', 'Pending approval', 'Rejected', 'Archived'];
 
 	const active = Data.filter(a => a.status === 'active');
 	const pending = Data.filter(a => a.status === 'pending approval');
 	const rejected = Data.filter(a => a.status === 'rejected');
 	const archived = Data.filter(a => a.status === 'archived');
+
+	const currentActive = active.slice(indexOfFirstRow, indexOfLastRow);
+	const currentPending = pending.slice(indexOfFirstRow, indexOfLastRow);
+	const currentRejected = rejected.slice(indexOfFirstRow, indexOfLastRow);
+	const currentArchived = archived.slice(indexOfFirstRow, indexOfLastRow);
+
+	const paginate = pageNumber => setCurrentPage(pageNumber);
 
 	return (
 		<Container>
@@ -31,10 +50,25 @@ const DataUsePage = () => {
 				<Tabs defaultActiveKey={'Active'} className='gray700-13'>
 					{tabs.map(tabName => (
 						<Tab eventKey={tabName} title={tabName}>
-							{tabName === 'Active' && <Table data={active} active={true} />}
-							{tabName === 'Pending approval' && <Table data={pending} />}
-							{tabName === 'Rejected' && <Table data={rejected} />}
-							{tabName === 'Archived' && <Table data={archived} />}
+							{tabName === 'Active' && <Table data={currentActive} active={true} />}
+							{tabName === 'Pending approval' && <Table data={currentPending} />}
+							{tabName === 'Rejected' && <Table data={currentRejected} />}
+							{tabName === 'Archived' && <Table data={currentArchived} />}
+							<Pagination
+								rowsPerPage={rowsPerPage}
+								totalRows={
+									tabName === 'Active'
+										? active.length
+										: tabName === 'Pending approval'
+										? pending.length
+										: tabName === 'Rejected'
+										? rejected.length
+										: tabName === 'Archived'
+										? archived.length
+										: row.length
+								}
+								paginate={paginate}
+							/>
 						</Tab>
 					))}
 				</Tabs>
--- src/pages/dataUse/DataUseTable.js
+++ src/pages/dataUse/DataUseTable.js
@@ -2,41 +2,43 @@ import React from 'react';
 import { Table, Dropdown } from 'react-bootstrap';
 
 const DataUseTable = ({ data, active }) => (
-	<Table striped bordered hover>
-		<thead>
-			<tr>
-				<th>Last activity</th>
-				<th>Project Title</th>
-				<th>Dataset(s)</th>
-				{active && <th></th>}
-			</tr>
-		</thead>
-		<tbody>
-			{data.map(a => (
+	<>
+		<Table striped bordered hover>
+			<thead>
 				<tr>
-					<td>{a.lastActivity}</td>
-					<td>
-						<p>{a.projectTitle}</p>
-						<p>{a.institution}</p>
-					</td>
-					<td>{a.datasets}</td>
-					{active && (
-						<td>
-							<Dropdown>
-								<Dropdown.Toggle variant='outline-secondary' id='dropdown-basic'>
-									Actions
-								</Dropdown.Toggle>
-								<Dropdown.Menu>
-									<Dropdown.Item href='#/action-1'>Edit</Dropdown.Item>
-									<Dropdown.Item href='#/action-2'>Archive</Dropdown.Item>
-								</Dropdown.Menu>
-							</Dropdown>
-						</td>
-					)}
+					<th>Last activity</th>
+					<th>Project Title</th>
+					<th>Dataset(s)</th>
+					{active && <th></th>}
 				</tr>
-			))}
-		</tbody>
-	</Table>
+			</thead>
+			<tbody>
+				{data.map(a => (
+					<tr>
+						<td>{a.lastActivity}</td>
+						<td>
+							<p>{a.projectTitle}</p>
+							<p>{a.institution}</p>
+						</td>
+						<td>{a.datasets}</td>
+						{active && (
+							<td>
+								<Dropdown>
+									<Dropdown.Toggle variant='outline-secondary' id='dropdown-basic'>
+										Actions
+									</Dropdown.Toggle>
+									<Dropdown.Menu>
+										<Dropdown.Item href='#/action-1'>Edit</Dropdown.Item>
+										<Dropdown.Item href='#/action-2'>Archive</Dropdown.Item>
+									</Dropdown.Menu>
+								</Dropdown>
+							</td>
+						)}
+					</tr>
+				))}
+			</tbody>
+		</Table>
+	</>
 );
 
 export default DataUseTable;
