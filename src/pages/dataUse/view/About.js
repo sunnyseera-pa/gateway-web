bad621af623aff9a47c6b9ec566297767d7f0eb4
umma-pa <umma.gohil@paconsulting.com>
2021-09-16 13:19:08 +0100

    Info icons and temp tooltips

--- src/pages/dataUse/view/About.js
+++ src/pages/dataUse/view/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import SVGIcon from '../../../images/SVGIcon';
 
 const About = ({ data }) => {
+	const exampleTooltip = props => (
+		<Tooltip className='datause-info-icon-tooltip' {...props}>
+			Example tooltip
+		</Tooltip>
+	);
+
 	return (
 		<>
 			{data.map(a => (
@@ -10,34 +17,74 @@ const About = ({ data }) => {
 						<p className='black-14-bold'>Safe people</p>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Organisation name</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.org}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Organisation ID</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safePeople.orgID}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Organisation sector</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safePeople.orgSector}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Applicant name(s)</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safePeople.applicantNames}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Applicant ID</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safePeople.applicantID}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Funders/Sponsor</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safePeople.funders}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>DEA accredited researcher status</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safePeople.accreditationStatus}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Sub-licence arrangements (if any)?</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safePeople.subLicence}</Col>
 						</Row>
 					</Container>
@@ -45,42 +92,92 @@ const About = ({ data }) => {
 						<p className='black-14-bold'>Safe projects</p>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Project ID</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.projectID}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Project title</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.title}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Lay summary</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.laySummary}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Public benefit statement</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.publicBenefitStatement}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Request category type</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.requestCategoryType}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Technical summary</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.technicalSummary}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Other approval committees</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.OtherApprovalCommittees}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Project start date</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.projectStartDate}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Project end date</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.projectEndDate}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Latest approval date</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeProjects.latestApprovalDate}</Col>
 						</Row>
 					</Container>
@@ -88,38 +185,83 @@ const About = ({ data }) => {
 						<p className='black-14-bold'>Safe data</p>
 						<Row className='soft-black-14'>
 							<Col>Dataset(s) name</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.datasetNames}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Data sensitivity level</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.dataSensitvityLevel}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Legal basis for provision of data</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.legalBasis}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Common law of duty of confidentiality</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.commonLawConfidentiality}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>National data opt-out applied?</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.nationalOpt}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Request frequency</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.requestFrequency}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Description of how the data will be used</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.descriptionUsage}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Description of the confidential data being used</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.descriptionConfidentialUsage}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Release/Access date</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeData.releaseAccessDate}</Col>
 						</Row>
 					</Container>
@@ -127,10 +269,20 @@ const About = ({ data }) => {
 						<p className='black-14-bold'>Safe setting</p>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>Trusted research environment or any other specified location</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeSetting.dataProcessPrivacy}</Col>
 						</Row>
 						<Row className='soft-black-14 datause-view-grid'>
 							<Col>How has data been processed to enhance privacy?</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeSetting.trustedResearchEnv}</Col>
 						</Row>
 					</Container>
@@ -138,18 +290,26 @@ const About = ({ data }) => {
 						<p className='black-14-bold'>Safe output</p>
 						<Row className='soft-black-14'>
 							<Col>Link to research outputs</Col>
+							<OverlayTrigger placement='top' overlay={exampleTooltip}>
+								<Button className='datause-info-icon-button'>
+									<SVGIcon name='info' width={8} height={8} fill={'#475da7'} className='datause-info-icon' />
+								</Button>
+							</OverlayTrigger>
 							<Col>{a.safeOutput.link}</Col>
 						</Row>
 					</Container>
 				</>
 			))}
-
 			<Row>
-				<p className='soft-black-14'>
-					Data custodians are responsible for providing information about their approved data uses register. Where not all fields are
-					completed, we hide empty fields to make the page easier to read, but you can choose to view them.
-				</p>
-				<Button>Hide all emptpy fields</Button>
+				<Col className='datause-about-info'>
+					<p className='soft-black-14'>
+						Data custodians are responsible for providing information about their approved data uses register. Where not all fields are
+						completed, we hide empty fields to make the page easier to read, but you can choose to view them.
+					</p>
+				</Col>
+			</Row>
+			<Row className='datause-hidefields-button'>
+				<Button>Show/Hide all empty fields</Button>
 			</Row>
 		</>
 	);
