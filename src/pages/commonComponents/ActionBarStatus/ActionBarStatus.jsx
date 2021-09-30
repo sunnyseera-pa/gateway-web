c12c3712764491f54b44aac60ae086602addadfb
Peter Hammans <peter.hammans@hdruk.co.uk>
2021-09-30 15:17:59 +0100

    Additon to readme

--- src/pages/commonComponents/ActionBarStatus/ActionBarStatus.jsx
+++ src/pages/commonComponents/ActionBarStatus/ActionBarStatus.jsx
@@ -5,65 +5,65 @@ import PropTypes from 'prop-types';
 import { DISPLAY_DATE_STANDARD } from '../../../configs/constants';
 import * as styles from './ActionBarStatus.styles';
 
-const formatDate = (date) => {
-    return moment(date).format(DISPLAY_DATE_STANDARD);
+const formatDate = date => {
+	return moment(date).format(DISPLAY_DATE_STANDARD);
 };
 
 const ActionBarStatus = ({ status, dataset, totalQuestions, ...outerProps }) => {
-    const {
-        timestamps: { published, submitted, rejected, archived },
-    } = dataset;
+	const {
+		timestamps: { published, submitted, rejected, archived },
+	} = dataset;
 
-    let content = null;
+	let content = null;
 
-    switch (status) {
-        case 'draft':
-            content = totalQuestions;
+	switch (status) {
+		case 'draft':
+			content = totalQuestions;
 
-            break;
-        case 'active':
-            content = `This version was published on ${formatDate(published)}`;
+			break;
+		case 'active':
+			content = `This version was published on ${formatDate(published)}`;
 
-            break;
-        case 'inReview':
-            content = `Submitted for review on ${formatDate(submitted)}`;
+			break;
+		case 'inReview':
+			content = `Submitted for review on ${formatDate(submitted)}`;
 
-            break;
-        case 'rejected':
-            content = `This version was rejected on ${formatDate(rejected)}`;
+			break;
+		case 'rejected':
+			content = `This version was rejected on ${formatDate(rejected)}`;
 
-            break;
-        case 'archived':
-            content = `This version was published on ${formatDate(published)} and archived on ${formatDate(archived)}`;
+			break;
+		case 'archived':
+			content = `This version was published on ${formatDate(published)} and archived on ${formatDate(archived)}`;
 
-            break;
-        default:
-            break;
-    }
+			break;
+		default:
+			break;
+	}
 
-    return (
-        <span {...outerProps} css={styles.root}>
-            {content}
-        </span>
-    );
+	return (
+		<div {...outerProps} className='action-bar-status' css={styles.root}>
+			{content}
+		</div>
+	);
 };
 
 ActionBarStatus.propTypes = {
-    dataset: PropTypes.shape({
-        timestamps: PropTypes.shape({
-            published: PropTypes.string,
-            submitted: PropTypes.string,
-            rejected: PropTypes.string,
-            archived: PropTypes.string,
-        }),
-    }),
-    totalQuestions: PropTypes.string,
-    status: PropTypes.oneOf(['draft', 'active', 'inReview', 'rejected', 'archived']),
-    className: PropTypes.string,
+	dataset: PropTypes.shape({
+		timestamps: PropTypes.shape({
+			published: PropTypes.string,
+			submitted: PropTypes.string,
+			rejected: PropTypes.string,
+			archived: PropTypes.string,
+		}),
+	}),
+	totalQuestions: PropTypes.string,
+	status: PropTypes.oneOf(['draft', 'active', 'inReview', 'rejected', 'archived']),
+	className: PropTypes.string,
 };
 
 ActionBarStatus.defaultProps = {
-    className: 'padding-md',
+	className: 'padding-md',
 };
 
 export default ActionBarStatus;
