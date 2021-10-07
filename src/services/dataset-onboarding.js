ca66759d1a9995c2df5f19d3b2b5eda451c7ec7e
Peter Hammans <peter.hammans@hdruk.co.uk>
2021-10-07 09:57:20 +0100

    Conflicts

--- src/services/dataset-onboarding.js
+++ src/services/dataset-onboarding.js
@@ -40,35 +40,20 @@ export const useGetDatasetOnboarding = (requestOptions, queryOptions = { queryKe
 	});
 };
 
-<<<<<<< HEAD
 const usePostDatasetOnboarding = (requestOptions, mutateOptions = { queryKey: 'postDatasetOnboarding' }) => {
 	return useMutation((_id, data) => postDatasetOnboarding(_id, data, requestOptions), {
-=======
-export const usePostDatasetOnboarding = (requestOptions, mutateOptions = { queryKey: 'postDatasetOnboarding' }) => {
-	return useMutation((id, data) => postDatasetOnboarding(id, data, requestOptions), {
->>>>>>> dev
 		mutateOptions,
 	});
 };
 
-<<<<<<< HEAD
 const usePutDatasetOnboarding = (requestOptions, mutateOptions = { queryKey: 'putDatasetOnboarding' }) => {
 	return useMutation((_id, data) => putDatasetOnboarding(_id, data, requestOptions), {
-=======
-export const usePutDatasetOnboarding = (requestOptions, mutateOptions = { queryKey: 'putDatasetOnboarding' }) => {
-	return useMutation((id, data) => putDatasetOnboarding(id, data, requestOptions), {
->>>>>>> dev
 		mutateOptions,
 	});
 };
 
-<<<<<<< HEAD
 const usePatchDatasetOnboarding = (requestOptions, mutateOptions = { queryKey: 'patchDatasetOnboarding' }) => {
 	return useMutation((_id, data) => patchDatasetOnboarding(_id, data, requestOptions), {
-=======
-export const usePatchDatasetOnboarding = (requestOptions, mutateOptions = { queryKey: 'patchDatasetOnboarding' }) => {
-	return useMutation((id, data) => patchDatasetOnboarding(id, data, requestOptions), {
->>>>>>> dev
 		mutateOptions,
 	});
 };
