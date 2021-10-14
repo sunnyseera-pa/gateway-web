0527666f89e2df0602ef60627af5ebae397a535d
umma-pa <umma.gohil@paconsulting.com>
2021-10-14 11:54:28 +0100

    Summary section

--- src/pages/dataUse/edit/EditDataUse.js
+++ src/pages/dataUse/edit/EditDataUse.js
@@ -1,10 +1,13 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, createRef } from 'react';
 import * as Sentry from '@sentry/react';
+import { Row, Col } from 'react-bootstrap';
+import SVGIcon from '../../../images/SVGIcon';
+import EditForm from './EditDataUseForm';
 import SearchBar from '../../commonComponents/searchBar/SearchBar';
 import ErrorModal from '../../commonComponents/errorModal/ErrorModal';
 
 const EditDataUse = props => {
-	const [searchBar] = useState(React.createRef());
+	const [searchBar] = useState(createRef());
 	const [searchString, setSearchString] = useState('');
 	const [showDrawer, setShowDrawer] = useState(false);
 	const [userState] = useState(
@@ -54,7 +57,32 @@ const EditDataUse = props => {
 				userState={userState}
 				doToggleDrawer={toggleDrawer}
 			/>
-			<div>Edit Data Use</div>
+			<div className='datause-card'>
+				<Row>
+					<Col md={10}>
+						<h5 className='black-20-semibold'>Edit a data use</h5>
+					</Col>
+					<Col md={2}>
+						<span className='badge-datause datause-badge-right'>
+							<SVGIcon name='datauseicon' fill={'#fff'} className='badgeSvg mr-2' viewBox='-2 -2 22 22' />
+							<span>Data use</span>
+						</span>
+					</Col>
+				</Row>
+				<Row>
+					<Col md={10}>
+						<p className='gray800-14-normal'>
+							A data use register is a public record of data an organisation has shared with other individuals and organisations. Data uses
+							help people understand how data is being used and why. Please edit data uses to ensure that the information is accurate and
+							up-to-date
+						</p>
+					</Col>
+				</Row>
+				<hr className='datause-border' />
+				<Row>
+					<EditForm />
+				</Row>
+			</div>
 		</Sentry.ErrorBoundary>
 	);
 };
---
+++ src/pages/dataUse/edit/EditDataUseForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EditForm = () => <div>Edit Data Use Form</div>;
+
+export default EditForm;
