f7cd1a340353ed110e0c6d1a2a50dc5d669d235b
umma-pa <umma.gohil@paconsulting.com>
2021-10-14 11:00:05 +0100

    Nav bar

--- src/pages/dataUse/edit/EditDataUse.js
+++ src/pages/dataUse/edit/EditDataUse.js
@@ -1,5 +1,62 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import * as Sentry from '@sentry/react';
+import SearchBar from '../../commonComponents/searchBar/SearchBar';
+import ErrorModal from '../../commonComponents/errorModal/ErrorModal';
 
-const EditDataUse = () => <div>Edit data use </div>;
+const EditDataUse = props => {
+	const [searchBar] = useState(React.createRef());
+	const [searchString, setSearchString] = useState('');
+	const [showDrawer, setShowDrawer] = useState(false);
+	const [userState] = useState(
+		props.userState || [
+			{
+				loggedIn: false,
+				role: 'Reader',
+				id: null,
+				name: null,
+			},
+		]
+	);
+
+	let showError = false;
+
+	const showModalHandler = () => {
+		showError = true;
+	};
+
+	const hideModalHandler = props => {
+		showError = false;
+	};
+
+	const doSearch = e => {
+		//fires on enter on searchbar
+		if (e.key === 'Enter') window.location.href = `/search?search=${encodeURIComponent(searchString)}`;
+	};
+
+	const updateSearchString = searchString => {
+		setSearchString(searchString);
+	};
+
+	const toggleDrawer = () => {
+		if (showDrawer === true) {
+			searchBar.current.getNumberOfUnreadMessages();
+		}
+		setShowDrawer(!showDrawer);
+	};
+
+	return (
+		<Sentry.ErrorBoundary fallback={<ErrorModal show={showModalHandler} handleClose={hideModalHandler} />}>
+			<SearchBar
+				ref={searchBar}
+				searchString={searchString}
+				doSearchMethod={doSearch}
+				doUpdateSearchString={updateSearchString}
+				userState={userState}
+				doToggleDrawer={toggleDrawer}
+			/>
+			<div>Edit Data Use</div>
+		</Sentry.ErrorBoundary>
+	);
+};
 
 export default EditDataUse;
