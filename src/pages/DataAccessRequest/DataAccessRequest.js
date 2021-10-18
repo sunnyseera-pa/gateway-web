d438a163b62dd79558477dec054f2a2692988b3d
samhPA <sam.haim@paconsulting.com>
2021-10-18 09:35:27 +0100

    bringing the data to the CSV. Next steps: (1) Bring the rest of the data (2) export createCSV to a seperate file

--- src/pages/DataAccessRequest/DataAccessRequest.js
+++ src/pages/DataAccessRequest/DataAccessRequest.js
@@ -1586,10 +1586,33 @@ class DataAccessRequest extends Component {
 
 	createCSV = (questionAnswers) => {
 		// set headers
-		const headers = ["ID", "Status", "Status notes",	"Date submitted", "Last activity", "Datasets", "Project name", "Project submitted by", "Applicant full name", "Job title", "Telephone", "Institutional afflilation", "ORCID", "Email", "Role", "Access to data", "Organisation name", "Line address 1",	"Line address 2", "Postcode", "City", "Country"];
-		// const row = JSON.stringify(questionAnswers);
+		const headers = ["ID", "Status", "Status notes", "Date submitted", "Last activity", "Datasets", "Project name", "Project submitted by", "Applicant full name", "Job title", "Telephone", "Institutional afflilation", "ORCID", "Email", "Role", "Access to data", "Organisation name", "Line address 1",	"Line address 2", "Postcode", "City", "Country"];
+		const row = [
+			"state._id",
+			"state.applicationStatus",
+			"Bring from screenData",
+			"Bring from screenData",
+			"Bring from screenData",
+			"Bring from screenData",
+			questionAnswers.applicationthisapplication,
+			questionAnswers.safeprojectapplicantfullname,
+			questionAnswers.safeprojectapplicantfullname,
+			questionAnswers.safeprojectapplicantjobtitle,
+			questionAnswers.safeprojectapplicanttelephone,
+			questionAnswers.safeprojectapplicantaffiliation,
+			questionAnswers.safeprojectapplicantorcid,
+			questionAnswers.safeprojectapplicantemail,
+			questionAnswers.safeprojectapplicantrole,
+			questionAnswers.safeprojectapplicantdataaccess,
+			questionAnswers.safeprojectorganisationname,
+			questionAnswers.safeprojectorganisationaddress1,
+			questionAnswers.safeprojectorganisationaddress2,
+			questionAnswers.safeprojectorganisationpostcode,
+			questionAnswers.safeprojectorganisationcity,
+			questionAnswers.safeprojectorganisationcountry,
+		];
 
-		return [headers];
+		return [headers, row];
 	}
 
 	render() {
