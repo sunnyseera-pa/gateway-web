17c9b5334dc5c8bd65b36f07d46e5414022ebfd7
samhPA <sam.haim@paconsulting.com>
2021-10-27 23:31:13 +0100

    refactoring: remove unneccessary code

--- src/pages/dashboard/DataAccessRequests/DataAccessRequests.js
+++ src/pages/dashboard/DataAccessRequests/DataAccessRequests.js
@@ -297,7 +297,6 @@ class DataAccessRequestsNew extends React.Component {
     )`${this.state.avgDecisionTime} average time from submission to decision`;
   };
 
-  // TO DO: Export this ?
   onClickDownloadCsv = async (team) => {
     
     // call the backend
@@ -316,57 +315,6 @@ class DataAccessRequestsNew extends React.Component {
 
     return;
   }
-
-  createCSV = (dataAccessRequests) => {
-
-    // set headers
-    const headers = this.buildHeaders();
-
-    // push the headers as the first row of the csv document
-    let csvRows = [ headers ];
-
-    // for each dataAccessRequest create csv rows and append it to the csv rows
-    for (const dar in dataAccessRequests) {
-      const rows = this.buildRows(dar);
-      csvRows.push(rows);
-    }
-
-    return csvRows
-  }
-
-  
-
-  buildRows = () => {
-    return [];
-  }
-
-  buildHeaders = () => {
-    return [
-          "ID", 
-          "Status", 
-          "Created At", 
-          "Updated At", 
-          "Datasets", 
-          "Project name", 
-          "Project submitted by", 
-          "Applicant full name", 
-          "Job title", 
-          "Telephone", 
-          "Institutional afflilation", 
-          "ORCID", 
-          "Email", 
-          "Role", 
-          "Access to data", 
-          "Organisation name", 
-          "Line address 1", 
-          "Line address 2", 
-          "Postcode", 
-          "City", 
-          "Country"
-        ];
-  }
-
-  // export till here ^
 
   render() {
     const {
