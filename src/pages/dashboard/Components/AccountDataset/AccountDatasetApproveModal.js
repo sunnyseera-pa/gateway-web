02f62dc53228460383e8fede641d3922dd23bf47
Sikenyonhdruk <simon.kenyon@hdruk.ac.uk>
2021-11-12 14:53:47 +0000

    Approval modal

--- src/pages/dashboard/Components/AccountDataset/AccountDatasetApproveModal.js
+++ src/pages/dashboard/Components/AccountDataset/AccountDatasetApproveModal.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, Form, Button } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { ReactComponent as CloseButtonSvg } from '../../../../images/close-alt.svg';
+import datasetOnboardingService from '../../../../services/dataset-onboarding/dataset-onboarding';
 import './AccountDatasetDecisionModal.scss';
 
 const AccountDatasetApproveModal = ({
@@ -10,6 +13,30 @@ const AccountDatasetApproveModal = ({
 	goToNext,
 	showGoToNext
 }) => {
+	const {
+			handleSubmit,
+			handleChange,
+			handleBlur,
+			touched,
+			values,
+			errors
+		} = useFormik({
+		initialValues: {
+			applicationStatusDesc: ''
+		},
+		validationSchema: Yup.object({
+			applicationStatusDesc: Yup.string(),
+		}),
+		onSubmit: async values => {
+			const payload = {
+				...values,
+				id,
+				applicationStatus: 'approved'
+			};
+			datasetOnboardingService.usePutDatasetOnboarding(payload);
+		},
+	});
+
 	return (
 		<Modal
 			show={open}
@@ -27,36 +54,51 @@ const AccountDatasetApproveModal = ({
 
 			<div className='account-dataset-decision-body'>
 				<div className='account-dataset-decision-body--wrap'>
-					<p></p>
+					<p>If you made any amendments to their application, such as fixing a type, please let the editor know using the description below.</p>
+					<label className='black-14'>Description (optional)</label>
+					<Form.Control
+						data-test-id='dataset-approve-applicationStatusDesc'
+						id='applicationStatusDesc'
+						name='applicationStatusDesc'
+						type='textarea'
+						className={touched.applicationStatusDesc && errors.applicationStatusDesc && 'save-modal-input'}
+						onChange={handleChange}
+						value={values.applicationStatusDesc}
+						onBlur={handleBlur}
+					/>
+					{touched.applicationStatusDesc && errors.applicationStatusDesc ? <div className='errorMessages'>{errors.applicationStatusDesc}</div> : null}
 				</div>
 			</div>
 			<div className='account-dataset-decision-footer'>
 				<div className='account-dataset-decision-footer--wrap'>
-					<button
+					<Button
 						className='button-secondary'
 						style={{ marginLeft: 'auto' }}
 						onClick={() => {
 							closed();
 						}}>
 						No, nevermind
-					</button>
-					<button
+					</Button>
+					<Button
 						className='button-secondary'
 						style={{ marginLeft: 'auto' }}
-						onClick={() => {
+						onClick={async () => {
+							handleSubmit();
 							closed();
 						}}>
 						Approve
-					</button>
-					<button
+					</Button>
+					<Button
+						disabled={!showGoToNext}
 						className='button-secondary'
 						style={{ marginLeft: 'auto' }}
 						onClick={() => {
+							handleSubmit();
 							goToNext();
 							closed();
 						}}>
 						Approve and go to next
-					</button>
+					</Button>
 				</div>
 			</div>
 		</Modal>
