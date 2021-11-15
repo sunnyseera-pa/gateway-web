cc8926f386d1531b45b21e74d63b50a322f4f152
Peter Hammans <peter.hammans@hdruk.co.uk>
2021-11-15 10:36:56 +0000

    Wrong prop name

--- src/services/activitylog/mockMsw.js
+++ src/services/activitylog/mockMsw.js
@@ -6,7 +6,7 @@ export const mockDatasetActivityLog = {
 	logs: [
 		{
 			version: 'Version 4.0.0',
-			datasetVersion: 4,
+			versionNumber: 4,
 			meta: {
 				dateSubmitted: '2021-09-03T09:51:44.842Z',
 				dateCreated: '2021-09-02T15:41:16.392Z',
@@ -16,7 +16,7 @@ export const mockDatasetActivityLog = {
 		},
 		{
 			version: 'Version 3.0.0',
-			datasetVersion: 3,
+			versionNumber: 3,
 			meta: {
 				dateSubmitted: '2021-09-02T15:38:29.571Z',
 				dateCreated: '2021-09-02T15:37:57.159Z',
@@ -26,7 +26,7 @@ export const mockDatasetActivityLog = {
 		},
 		{
 			version: 'Version 2.0.0',
-			datasetVersion: 2,
+			versionNumber: 2,
 			meta: {
 				dateSubmitted: '2021-09-02T14:47:48.056Z',
 				dateCreated: '2021-09-02T14:44:08.889Z',
@@ -36,7 +36,7 @@ export const mockDatasetActivityLog = {
 		},
 		{
 			version: 'Version 1.0.0',
-			datasetVersion: 1,
+			versionNumber: 1,
 			meta: {
 				dateSubmitted: '2021-09-02T13:29:59.320Z',
 				dateCreated: '2021-09-02T13:26:05.761Z',
