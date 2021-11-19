0aeb59bc93578ea37a835416c936934c2452b319
Peter Hammans <peter.hammans@hdruk.co.uk>
2021-11-19 09:03:34 +0000

    Temporary SortDropdown

--- src/pages/commonComponents/SortDropdown/SortDropdown.js
+++ src/pages/commonComponents/SortDropdown/SortDropdown.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
 import Icon from '../Icon';
 
 const SortDropdown = ({ handleSort, defaultValue, value, options, iconSelected }) => {
-	const { t } = useTranslation();
 	const currentValue = !!value ? defaultValue : value;
 
 	return (
 		<Dropdown className='ui-SortDropdown' alignRight onSelect={handleSort}>
 			<Dropdown.Toggle variant='info'>
-				{/* {(() => {
+				{(() => {
 					if (sorting === 'popularity') return 'Sort by number of views';
 					else if (sorting === 'metadata') return 'Sort by metadata quality';
 					else if (sorting === 'resources') return 'Sort by related resources';
@@ -18,15 +16,13 @@ const SortDropdown = ({ handleSort, defaultValue, value, options, iconSelected }
 					else if (sorting === 'recentlyadded') return 'Sort by recently added';
 					else if (sorting === 'sortbyyear') return 'Sort by year';
 					else return 'Sort by match to search terms';
-				})()} */}
-				{t(`sort.label.${currentValue}`)}
+				})()}
 			</Dropdown.Toggle>
 			<Dropdown.Menu>
 				{options.map(item => {
 					return (
 						<Dropdown.Item eventKey={item} className='gray800-14'>
-							{t(`sort.option.${item}`)}
-							{/* {(() => {
+							{(() => {
 								if (item === 'popularity') return 'Number of views (highest to lowest)';
 								else if (item === 'metadata') return 'Metadata quality (platinum to bronze)';
 								else if (item === 'resources') return 'Related resources (most first)';
@@ -34,7 +30,7 @@ const SortDropdown = ({ handleSort, defaultValue, value, options, iconSelected }
 								else if (item === 'recentlyadded') return 'Recently added to collection (newest first)';
 								else if (item === 'relevance') return 'Match to search terms (closest first)';
 								else if (item === 'sortbyyear') return 'Sort by year (latest first)';
-							})()} */}
+							})()}
 							{item === currentValue && iconSelected}
 						</Dropdown.Item>
 					);
