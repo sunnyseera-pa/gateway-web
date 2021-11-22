2d8bc49768ae39d93fecd5fe81f0ef88073a427f
Peter Hammans <peter.hammans@hdruk.co.uk>
2021-11-22 08:27:21 +0000

    Change function name

--- src/hooks/useDomChanged.js
+++ src/hooks/useDomChanged.js
@@ -7,7 +7,7 @@ const useDOMChanged = ref => {
 	});
 
 	useEffect(() => {
-		const setWidths = () => {
+		const initValuesChanged = () => {
 			const updatedValues = Object.keys(values).reduce((previousValue, currentValue) => {
 				return {
 					...previousValue,
@@ -18,10 +18,10 @@ const useDOMChanged = ref => {
 			if (!isEqual(values)) setValues(updatedValues);
 		};
 
-		if (ref.current) ref.current.addEventListener('DOMSubtreeModified', setWidths);
+		if (ref.current) ref.current.addEventListener('DOMSubtreeModified', initValuesChanged);
 
 		return () => {
-			if (ref.current) ref.current.removeEventListener('DOMSubtreeModified', setWidths);
+			if (ref.current) ref.current.removeEventListener('DOMSubtreeModified', initValuesChanged);
 		};
 	}, [ref.current]);
 
