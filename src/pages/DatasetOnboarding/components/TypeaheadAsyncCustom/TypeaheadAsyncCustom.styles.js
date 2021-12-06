8d737c0e14e344333d602f8e78ae2bc10a694a22
kandaj <jagi.work@gmail.com>
2021-12-06 09:59:42 +0000

    search icon added

--- src/pages/DatasetOnboarding/components/TypeaheadAsyncCustom/TypeaheadAsyncCustom.styles.js
+++ src/pages/DatasetOnboarding/components/TypeaheadAsyncCustom/TypeaheadAsyncCustom.styles.js
@@ -7,3 +7,21 @@ export const hierarchy = css`
 	float: right;
 	color: grey;
 `;
+
+export const icons = css`
+	align-items: center;
+	display: flex;
+	bottom: 0;
+	justify-content: center;
+	pointer-events: none;
+	position: absolute;
+	right: 0;
+	top: 0;
+	width: 34px;
+	left: -95%;
+	width: auto;
+`;
+
+export const cursor = show => css`
+	${show ? ` padding: 5px 0 0 25px;` : `padding: 5px 0 0 0;`}
+`;
