c8768ff7bd35b7f0f0411a73dcfebdf664bf9076
umma-pa <umma.gohil@paconsulting.com>
2021-12-07 16:35:29 +0000

    Nested functionality fix

--- src/pages/DataAccessRequest/components/DoubleDropdownCustom/DoubleDropdownCustom.js
+++ src/pages/DataAccessRequest/components/DoubleDropdownCustom/DoubleDropdownCustom.js
@@ -21,9 +21,35 @@ const DoubleDropdownCustom = ({ name, id, options, input, onChange, labelId, req
 			<Nav className='mr-auto'>
 				<Dropdown onSelect={changingSelect}>
 					<Dropdown.Toggle className='double-dropdown-input'>{selectedValue}</Dropdown.Toggle>
-
 					<Dropdown.Menu>
-						{options.map(a => (
+						{options.map(b =>
+							b.value === 'Biomedical research' ? (
+								<Dropdown className='nested-dropdown-whole'>
+									<Dropdown.Toggle className='nested-dropdown'>
+										Biomedical research
+										<SVGIcon
+											width='20px'
+											height='20px'
+											name='chevronbottom'
+											fill={'#475da7'}
+											className={closed ? 'chevron nest-dropdown-arrow' : 'chevron flip180 nest-dropdown-arrow'}
+										/>
+									</Dropdown.Toggle>
+
+									<Dropdown.Menu className='nested-dropdown-menu'>
+										{extra.map(a => (
+											<Dropdown.Item eventKey={a.text} onSelect={changingSelect}>
+												{a.value}
+											</Dropdown.Item>
+										))}
+									</Dropdown.Menu>
+								</Dropdown>
+							) : (
+								<Dropdown.Item eventKey={b.value}>{b.value} </Dropdown.Item>
+							)
+						)}
+
+						{/*options.map(a => (
 							<Dropdown.Item eventKey={a.value}>
 								{a.value}
 								{a.value === 'Biomedical research' && (
@@ -41,12 +67,14 @@ const DoubleDropdownCustom = ({ name, id, options, input, onChange, labelId, req
 								{a.value === 'Biomedical research' && !closed && (
 									<Dropdown.Menu className='nested-dropdown-menu'>
 										{extra.map(a => (
-											<Dropdown.Item eventKey={a.text}>{a.value}</Dropdown.Item>
+											<Dropdown.Item eventKey={a.text} onSelect={changingSelect}>
+												{a.value}
+											</Dropdown.Item>
 										))}
 									</Dropdown.Menu>
 								)}
 							</Dropdown.Item>
-						))}
+										))*/}
 					</Dropdown.Menu>
 				</Dropdown>
 			</Nav>
@@ -55,3 +83,85 @@ const DoubleDropdownCustom = ({ name, id, options, input, onChange, labelId, req
 };
 
 export default DoubleDropdownCustom;
+
+/*
+<Dropdown.Item>Age category research</Dropdown.Item>
+						<Dropdown.Item>Ancestry research</Dropdown.Item>
+						<Dropdown className='nested-dropdown-whole'>
+							<Dropdown.Toggle className='nested-dropdown'>
+								Biomedical research
+								<SVGIcon
+									width='20px'
+									height='20px'
+									name='chevronbottom'
+									fill={'#475da7'}
+									className={closed ? 'chevron nest-dropdown-arrow' : 'chevron flip180 nest-dropdown-arrow'}
+								/>
+							</Dropdown.Toggle>
+
+							<Dropdown.Menu className='nested-dropdown-menu'>
+								<Dropdown.Item>Disease category research</Dropdown.Item>
+								<Dropdown.Item>Drug development research</Dropdown.Item>
+								<Dropdown.Item>Gentic research</Dropdown.Item>
+							</Dropdown.Menu>
+						</Dropdown>
+						<Dropdown.Item>Gender category research</Dropdown.Item>
+						<Dropdown.Item>Method development</Dropdown.Item>
+						<Dropdown.Item>Population research</Dropdown.Item>
+						<Dropdown.Item>Research control</Dropdown.Item> */
+
+/*
+return (
+		<Navbar collapseOnSelect expand='lg'>
+			<Nav className='mr-auto'>
+				<Dropdown>
+					<Dropdown.Toggle className='double-dropdown-input'>
+						<Typeahead
+							name={name}
+							id={id}
+							multiple
+							aria-labelledby={labelId}
+							//className={props.classes.input}
+							required={required ? 'required' : undefined}
+							type='text'
+							onChange={e => handleChange(e)}
+							//onBlur={handleBlur}
+							//onFocus={handleFocus}
+							options={schema}
+							labelKey='value'
+							//selected={this.props.value}
+							//data-test-id='darContributorTextInput'
+						/>
+					</Dropdown.Toggle>
+
+					{/*<Dropdown.Menu>
+						<Dropdown.Item>Age category research</Dropdown.Item>
+						<Dropdown.Item>Ancestry research</Dropdown.Item>
+						<Dropdown className='nested-dropdown-whole'>
+							<Dropdown.Toggle className='nested-dropdown'>
+								Biomedical research
+								<SVGIcon
+									width='20px'
+									height='20px'
+									name='chevronbottom'
+									fill={'#475da7'}
+									className={closed ? 'chevron nest-dropdown-arrow' : 'chevron flip180 nest-dropdown-arrow'}
+								/>
+							</Dropdown.Toggle>
+
+							<Dropdown.Menu className='nested-dropdown-menu'>
+								<Dropdown.Item>Disease category research</Dropdown.Item>
+								<Dropdown.Item>Gentic research</Dropdown.Item>
+							</Dropdown.Menu>
+						</Dropdown>
+						<Dropdown.Item>Gender category research</Dropdown.Item>
+						<Dropdown.Item>Method development</Dropdown.Item>
+						<Dropdown.Item>Population research</Dropdown.Item>
+						<Dropdown.Item>Research control</Dropdown.Item>
+					</Dropdown.Menu>}
+					</Dropdown>
+					</Nav>
+				</Navbar>
+			);
+		};
+		 */
