c5c323c8af1ac1ca721dc02c33c1637a769d72f9
samhPA <sam.haim@paconsulting.com>
2021-12-13 16:05:00 +0000

    added the menu but still has a bug on the backend when I click <<amend form>>

--- src/pages/DataAccessRequest/components/ApplicantActionButtons/ApplicantActionButtons.js
+++ src/pages/DataAccessRequest/components/ApplicantActionButtons/ApplicantActionButtons.js
@@ -16,6 +16,8 @@ const ApplicantActionButtons = ({
 	onDuplicateClick,
 	onShowAmendApplicationModal,
 }) => {
+	console.log("isClonable", isCloneable);
+	console.log("applicationStatus", applicationStatus);
 	const options = [
 		{
 			description: 'Manage application:',
@@ -70,21 +72,8 @@ const ApplicantActionButtons = ({
 
 	return (
 		<Fragment>
-			{applicationStatus === DarHelper.darStatus.inProgress ? (
-				<button className={`btn-link btn-link-delete p-2 ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onDeleteDraftClick()}>
-					Delete draft
-				</button>
-			) : (
-				''
-			)}
-			{allowedNavigation && isCloneable && (
-				<button className={`button-tertiary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onDuplicateClick()}>
-					Duplicate
-				</button>
-			)}
-			<button className={`button-tertiary ${allowedNavigation ? '' : 'disabled'}`} onClick={e => onShowContributorModal()}>
-				Contributors
-			</button>
+			<ActionBarMenu label='Manage application' options={availableOptions} disabled={!allowedNavigation} buttonClass='button-tertiary' />
+
 			{showSubmit && (
 				<button
 					className={`button-secondary ${allowedNavigation ? '' : 'disabled'}`}
