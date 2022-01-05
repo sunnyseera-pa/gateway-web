9be0fe1705b4226e020b1bc578996109bcea32b6
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-01-05 16:23:10 +0000

    Mixins

--- src/components/Checkbox/Checkbox.styles.js
+++ src/components/Checkbox/Checkbox.styles.js
@@ -9,6 +9,32 @@ export const root = ({ variant }) => theme => {
 	} = theme;
 
 	return css`
+		${mixins.root({ width })}
+
+		&::before {
+			${mixins.before({ colors, variants, variant, width, height })}
+		}
+
+		input + .ui-Checkbox__label::after {
+			${mixins.after({ width, height })}
+		}
+
+		input:disabled:not(:checked) + .ui-Checkbox__label::after {
+			${mixins.disabledNotChecked({ width, height })}
+		}
+
+		input:checked + .ui-Checkbox__label::after {
+			${mixins.checked({ colors, variants, variant })}
+		}
+
+		input:disabled + .ui-Checkbox__label::after {
+			${mixins.disabled({ colors, variants, variant })}
+		}
+	`;
+};
+
+export const mixins = {
+	root: ({ width }) => `
 		position: relative;
 		padding-left: calc(${width} + 0.5rem);
 		padding-top: 2px;
@@ -20,43 +46,38 @@ export const root = ({ variant }) => theme => {
 		&:hover {
 			cursor: pointer;
 		}
-
-		&::before {
-			content: '';
-			position: absolute;
-			top: 0;
-			left: 0;
-			border-width: 2px;
-			border-style: solid;
-			border-color: ${colors[variants[variant].borderColor]};
-			width: ${width};
-			height: ${height};
-		}
-
-		input + .ui-Checkbox__label::after {
-			content: '';
-			position: absolute;
-			top: 5px;
-			left: 5px;
-			width: calc(${width} - 10px);
-			height: calc(${height} - 10px);
-		}
-
-		input:disabled:not(:checked) + .ui-Checkbox__label::after {
-			content: '';
-			position: absolute;
-			top: 2px;
-			left: 2px;
-			width: calc(${width} - 4px);
-			height: calc(${height} - 4px);
-		}
-
-		input:checked + .ui-Checkbox__label::after {
-			background: ${colors[variants[variant].checkedBackground]};
-		}
-
-		input:disabled + .ui-Checkbox__label::after {
-			background: ${colors[variants[variant].backgroundDisabled]};
-		}
-	`;
+	`,
+	before: ({ colors, variants, variant, width, height }) => `
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		border-width: 2px;
+		border-style: solid;
+		border-color: ${colors[variants[variant].borderColor]};
+		width: ${width};
+		height: ${height};
+	`,
+	after: ({ width, height }) => `
+		content: '';
+		position: absolute;
+		top: 5px;
+		left: 5px;
+		width: calc(${width} - 10px);
+		height: calc(${height} - 10px);
+	`,
+	disabledNotChecked: ({ width, height }) => `
+		content: '';
+		position: absolute;
+		top: 2px;
+		left: 2px;
+		width: calc(${width} - 4px);
+		height: calc(${height} - 4px);
+	`,
+	checked: ({ colors, variants, variant }) => `
+		background: ${colors[variants[variant].checkedBackground]};
+	`,
+	disabled: ({ colors, variants, variant }) => `
+		background: ${colors[variants[variant].backgroundDisabled]};
+	`,
 };
--- src/components/CheckboxTree/CheckboxTree.styles.js
+++ src/components/CheckboxTree/CheckboxTree.styles.js
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import { mixins } from '../Checkbox/Checkbox.styles';
 
 export const root = ({ variant = 'primary', hasLeafIcon, hasParentIcon }) => theme => {
 	const {
@@ -10,9 +11,7 @@ export const root = ({ variant = 'primary', hasLeafIcon, hasParentIcon }) => the
 	} = theme;
 
 	return css`
-		position: relative;
-		padding-left: calc(${width} + 0.5rem);
-		padding-top: 2px;
+		${mixins.root({ width })}
 
 		${!hasParentIcon &&
 		`.rct-node-parent > .rct-text .rct-node-icon {
@@ -23,10 +22,6 @@ export const root = ({ variant = 'primary', hasLeafIcon, hasParentIcon }) => the
 		`.rct-node-leaf > .rct-text .rct-node-icon {
 				display: none;
 			}`}
-
-		input {
-			display: none;
-		}
 
 		label {
 			display: flex;
@@ -58,10 +53,6 @@ export const root = ({ variant = 'primary', hasLeafIcon, hasParentIcon }) => the
 			padding: 0 0 ${2 * increment}px 0;
 		}
 
-		&:hover {
-			cursor: pointer;
-		}
-
 		input + .rct-checkbox {
 			position: relative;
 			width: ${width};
@@ -69,41 +60,23 @@ export const root = ({ variant = 'primary', hasLeafIcon, hasParentIcon }) => the
 		}
 
 		input + .rct-checkbox .rct-icon::before {
-			content: '';
-			position: absolute;
-			top: 0;
-			left: 0;
-			border-width: 2px;
-			border-style: solid;
-			border-color: ${colors[variants[variant].borderColor]};
-			width: ${width};
-			height: ${height};
+			${mixins.before({ colors, variants, variant, width, height })}
 		}
 
 		input + .rct-checkbox .rct-icon::after {
-			content: '';
-			position: absolute;
-			top: 5px;
-			left: 5px;
-			width: calc(${width} - 10px);
-			height: calc(${height} - 10px);
+			${mixins.after({ width, height })}
 		}
 
 		input:disabled + .rct-checkbox .rct-icon-uncheck::after {
-			content: '';
-			position: absolute;
-			top: 2px;
-			left: 2px;
-			width: calc(${width} - 4px);
-			height: calc(${height} - 4px);
+			${mixins.disabledNotChecked({ width, height })}
 		}
 
 		input + .rct-checkbox .rct-icon-check::after {
-			background: ${colors[variants[variant].checkedBackground]};
+			${mixins.checked({ colors, variants, variant })}
 		}
 
 		input:disabled + .rct-checkbox .rct-icon::after {
-			background: ${colors[variants[variant].backgroundDisabled]};
+			${mixins.disabled({ colors, variants, variant })}
 		}
 	`;
 };
