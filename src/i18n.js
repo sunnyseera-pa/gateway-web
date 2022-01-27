9dbb30b8d1e5a5ea14f7a63359b3d02e76b70964
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-01-27 00:12:03 +0000

    Conditional import of translations

--- src/i18n.js
+++ src/i18n.js
@@ -3,30 +3,28 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-import enGBTranslation from 'public/locales/en-GB/translation.json';
+const config = {
+	debug: false,
+	fallbackLng: 'en-GB',
+	lng: 'en-GB',
+	interpolation: {
+		escapeValue: false,
+	},
+};
 
-i18n
-	.use(Backend)
-	.use(LanguageDetector)
-	.use(initReactI18next)
-	.init({
-		debug: false,
-		fallbackLng: 'en-GB',
-		lng: 'en-GB',
-		interpolation: {
-			escapeValue: false,
-		},
-		...(process.env.NODE_ENV === 'test' && {
-			react: {
-				wait: true,
-				useSuspense: false,
-			},
+if (process.env.NODE_ENV === 'test') {
+	import('public/locales/en-GB/translation.json').then(enGBTranslation => {
+		i18n.use(initReactI18next).init({
+			...config,
 			resources: {
 				'en-GB': {
 					translation: enGBTranslation,
 				},
 			},
-		}),
+		});
 	});
+} else {
+	i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init(config);
+}
 
 export default i18n;
