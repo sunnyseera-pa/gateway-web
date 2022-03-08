21bb2b36f24d72ab8cf86d42c9bd29637f77b0ab
Peter Hammans <peter.hammans@hdruk.co.uk>
2022-03-08 14:01:28 +0000

    Spacing

--- src/components/SearchControls/SearchControlsForm.js
+++ src/components/SearchControls/SearchControlsForm.js
@@ -6,79 +6,79 @@ import SearchInput from '../SearchInput';
 import SortDropdown from '../SortDropdown';
 import useCommonStyles from '../../hooks/useCommonStyles';
 
-const SearchControlsSortForm = ({ sortProps, inputProps, type, mt, mb, mr, ml, width, minWidth, maxWidth }) => {
-	const { t } = useTranslation();
-	const commonStyles = useCommonStyles({ mt, mb, mr, ml, width, minWidth, maxWidth });
+const SearchControlsForm = ({ sortProps, inputProps, type, mt, mb, mr, ml, width, minWidth, maxWidth }) => {
+    const { t } = useTranslation();
+    const commonStyles = useCommonStyles({ mt, mb, mr, ml, width, minWidth, maxWidth });
 
-	const { direction, value } = sortProps;
+    const { direction, value } = sortProps;
 
-	const {
-		submitForm,
-		resetForm,
-		values: { search, sortBy, sortDirection },
-		values,
-		setFieldValue,
-	} = useFormikContext();
+    const {
+        submitForm,
+        resetForm,
+        values: { search, sortBy, sortDirection },
+        values,
+        setFieldValue,
+    } = useFormikContext();
 
-	const handleKeyDown = React.useCallback(e => {
-		if (e.key === 'Enter') {
-			if (inputProps.onKeyDownEnter) inputProps.onKeyDownEnter(submitForm);
-		}
-	}, []);
+    const handleKeyDown = React.useCallback(e => {
+        if (e.key === 'Enter') {
+            if (inputProps.onKeyDownEnter) inputProps.onKeyDownEnter(submitForm);
+        }
+    }, []);
 
-	const handleReset = React.useCallback(() => {
-		resetForm();
+    const handleReset = React.useCallback(() => {
+        resetForm();
 
-		if (inputProps.onReset) inputProps.onReset(submitForm);
-	}, [values]);
+        if (inputProps.onReset) inputProps.onReset(submitForm);
+    }, [values]);
 
-	const handleChange = React.useCallback(({ target: { name, value } }) => {
-		setFieldValue(name, value);
+    const handleChange = React.useCallback(({ target: { name, value } }) => {
+        setFieldValue(name, value);
 
-		const { onChange } = inputProps;
+        const { onChange } = inputProps;
 
-		if (onChange) onChange(value);
-	}, []);
+        if (onChange) onChange(value);
+    }, []);
 
-	const handleOnSort = React.useCallback(
-		criteria => {
-			const { value, direction } = criteria;
-			const { onSort } = sortProps;
+    const handleOnSort = React.useCallback(
+        criteria => {
+            const { value, direction } = criteria;
+            const { onSort } = sortProps;
 
-			setFieldValue('sortBy', value);
-			setFieldValue('sortDirection', direction);
+            setFieldValue('sortBy', value);
+            setFieldValue('sortDirection', direction);
 
-			if (onSort) onSort(criteria, submitForm);
-		},
-		[values]
-	);
+            if (onSort) onSort(criteria, submitForm);
+        },
+        [values]
+    );
 
-	React.useEffect(() => {
-		setFieldValue('sortBy', value);
-		setFieldValue('sortDirection', direction);
-	}, [value, direction]);
+    React.useEffect(() => {
+        setFieldValue('sortBy', value);
+        setFieldValue('sortDirection', direction);
+    }, [value, direction]);
 
-	return (
-		<Row className={commonStyles}>
-			<Col lg={6}>
-				<SearchInput
-					placeholder={t('search.placeholder', { type: type.toLowerCase() })}
-					variant='primary'
-					{...inputProps}
-					value={search}
-					onKeyDown={handleKeyDown}
-					onChange={handleChange}
-					onReset={handleReset}
-				/>
-			</Col>
-			<Col lg={2} />
-			{sortProps && (
-				<Col lg={4} className='d-flex justify-content-end'>
-					<SortDropdown {...sortProps} onSort={handleOnSort} value={sortBy} direction={sortDirection} />
-				</Col>
-			)}
-		</Row>
-	);
+    return (
+        <Row className={commonStyles}>
+            <Col lg={6}>
+                <SearchInput
+                    placeholder={t('search.placeholder', { type: type.toLowerCase() })}
+                    variant='primary'
+                    {...inputProps}
+                    value={search}
+                    onKeyDown={handleKeyDown}
+                    onChange={handleChange}
+                    onReset={handleReset}
+                />
+            </Col>
+            <Col lg={2} />
+            {sortProps && (
+                <Col lg={4} className='d-flex justify-content-end'>
+                    <SortDropdown {...sortProps} onSort={handleOnSort} value={sortBy} direction={sortDirection} />
+                </Col>
+            )}
+        </Row>
+    );
 };
 
-export default SearchControlsSortForm;
+export default SearchControlsForm;
